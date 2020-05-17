143778514975d31423b761a4152d774eacf7251c
opianykh <elena.pyanyh@gmail.com>
2020-05-18 00:09:16 +0300

    Added Footer Task

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from '../header/Header';
+// todo: здесь нужно сделать импорт Footer из '../footer/Footer'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <div>content will appear here soon</div>
+    {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
     </div>
   );
 }
