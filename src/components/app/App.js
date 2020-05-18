6627945c5d5e12ace2e9ea39aac97d33c815b89a
xeilen <xeilen1112@gmail.com>
2020-05-18 23:53:01 +0300

    -HomeWork 1 status: done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
+import Footer from "../footer/Footer";
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
+import {usersList} from "../../constants";
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import UserCard from "../user-card/UserCard";
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,8 +16,9 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <UserCard user={usersList[0]}/>
       <div>content will appear here soon</div>
-      {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
+      <Footer />
 
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
     </div>
