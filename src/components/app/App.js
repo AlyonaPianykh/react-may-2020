71a8df652c369c94ee7980f08e794d3cb7eaf4bf
opianykh <elena.pyanyh@gmail.com>
2020-05-18 21:53:22 +0300

    Lecture 1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from '../header/Header';
+import { Header } from '../header/HeaderFromLecture';
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
