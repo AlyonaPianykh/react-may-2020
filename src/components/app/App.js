cb0392ef05b22e55d293f26d75f3ad4d7f3973b1
ArtemMachulenko <dr.a.mefesto@gmail.com>
2020-05-19 11:38:54 +0300

    -finished task1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
-
+import { Footer } from '../footer/Footer';
 // todo: здесь нужно сделать импорт usersList из '../../constants'
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+
+
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,7 +17,7 @@ function App() {
       <Header />
       <div>content will appear here soon</div>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-
+       <Footer />
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
     </div>
   );
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 //todo здесь нужно сделать импорт socialMediaIcons из файла '../../constants'
+import { socialMediaIcons } from '../../constants';
 //todo здесь нужно сделать импорт стилей из './Footer.scss'
+import './Footer.scss';
 
-
+export const Footer = (props) => {
+    return (
+        <div className="may-footer">
+            <div className="may-footer-content">
+                <div>Some text about authors</div>
+                <div className="may-footer-social-media-wrapper">
+                    <img src={socialMediaIcons[0].src} className="may-footer-social-media-icon" alt={socialMediaIcons[0].alt}/>
+                    <img src={socialMediaIcons[1].src} className="may-footer-social-media-icon" alt={socialMediaIcons[1].alt}/>
+                    <img src={socialMediaIcons[2].src} className="may-footer-social-media-icon" alt={socialMediaIcons[2].alt}/>
+                </div>
+            </div>
+            <div>All rights reserved. may-app © 2020</div>
+        </div>
+    );
+}
 
   // todo здесь нужно сделать экспорт функии под названием Footer
   //   она должна вернуть div с классом "may-footer"
