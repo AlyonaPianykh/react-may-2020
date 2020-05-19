0248f798db5f38e64f4a7e4dd84626bb78335475
PashaShevchuk <pasha608@gmail.com>
2020-05-19 22:14:23 +0300

    - completed task 1

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -19,9 +19,11 @@ export const Footer = () => {
             <div className='may-footer-content'>
                 <div>Some text about authors</div>
                 <div className='may-footer-social-media-wrapper'>
-                    <img src={socialMediaIcons[0].src} alt={socialMediaIcons[0].alt} className='may-footer-social-media-icon'/>
-                    <img src={socialMediaIcons[1].src} alt={socialMediaIcons[1].alt} className='may-footer-social-media-icon'/>
-                    <img src={socialMediaIcons[2].src} alt={socialMediaIcons[2].alt} className='may-footer-social-media-icon'/>
+                    {socialMediaIcons.map((value, index) => {
+                        return (
+                            <img key={index} src={value.src} alt={value.alt} className='may-footer-social-media-icon'/>
+                        );
+                    })}
                 </div>
             </div>
             <div>All rights reserved. may-app © 2020</div>
