6f0118b47edfcdae55f2969325d812ef296b4b0a
Andrej-oss <snowtomsawyer@gmail.com>
2020-05-19 17:24:01 +0200

    -started from master branch. Just want to start from the beginning

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import "../Header/header.scss"
 import './App.css';
+import {Header} from "../Header/header";
+import {Footer} from "../footer/Footer";
+import {UserCard} from "../UserCard/UserCard";
+import {usersList} from "../../Contetnt/Index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+          <UserCard user={usersList[0]} />
+        <Footer/>
     </div>
   );
 }
