0207ef914c96fb34c5935a9137aedfd684277876
vkhomyn95 <vkhomyn95@gmail.com>
2020-05-19 12:48:44 +0300

    hometask1

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 //todo здесь нужно сделать импорт socialMediaIcons из файла '../../constants'
+import {socialMediaIcons} from '../../constants';
 //todo здесь нужно сделать импорт стилей из './Footer.scss'
-
+import './Footer.scss';
 
 
   // todo здесь нужно сделать экспорт функии под названием Footer
+
+export const Footer = (props) => {
+    console.log(socialMediaIcons);
+    return (
+       <div className="may-footer">
+           <div className="may-footer-content">
+               <div>Some text about authors</div>
+               <div className="may-footer-social-media-wrapper">
+                   <img className="may-footer-social-media-icon" src={socialMediaIcons[0].src} alt={socialMediaIcons[0].alt}/>
+                   <img className="may-footer-social-media-icon" src={socialMediaIcons[1].src} alt={socialMediaIcons[1].alt}/>
+                   <img className="may-footer-social-media-icon" src={socialMediaIcons[2].src} alt={socialMediaIcons[2].alt}/>
+               </div>
+           </div>
+           <div>All rights reserved. may-app © 2020</div>
+       </div>
+    );
+};
   //   она должна вернуть div с классом "may-footer"
   //   в котором будет 2 div блока: 1й с классом "may-footer-content", второй будет без класса но с текстом 'All rights reserved. may-app © 2020'
   //   в первом div блоке должны быть вложены еще 2 блока div: 1й с текстом "Some text about authors"
