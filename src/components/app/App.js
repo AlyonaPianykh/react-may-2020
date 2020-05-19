d5c316db34f148eea45782e4851af5cc4d5d24f8
bohdanbolyubash <bbolyubash@icloud.com>
2020-05-19 17:24:50 +0300

    - i created footer - i created user card

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
+import {Footer} from "../footer/Footer";
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
+import {usersList} from '../../constants';
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import {UserCard} from '../user-card/UserCard'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,8 +18,9 @@ function App() {
       <Header />
       <div>content will appear here soon</div>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-
+    <Footer/>
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
+      <UserCard user={usersList[0]}> </UserCard>
     </div>
   );
 }
