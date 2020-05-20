cf1f98013a3eddcee87285bc239958aca8fafeda
opianykh <elena.pyanyh@gmail.com>
2020-05-20 21:51:41 +0300

    Lecture 2

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
+
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
+import TestCard, { PostCard as Card } from '../post-card/PostCard';
+import { postsList } from '../../constants';
 
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const renderList = () => {
+    const res = [];
+
+    for (let i = 0; i < 6; i++) {
+      const item = postsList[i];
+      res.push(<Card post={item} key={item.id}/>)
+    }
+
+    return res;
+  };
+
+  const text = '';
+
   return (
     <div className="App">
       <Header />
       <div>content will appear here soon</div>
-      <Footer/>
+
+      {/*<div className="d-flex posts-container">*/}
+      {/*  {*/}
+      {/*    renderList()*/}
+      {/*  }*/}
+      {/*</div>*/}
+
+      <div className="d-flex posts-container">
+        {
+          postsList.map((item, index) => {
+            const odd = index % 2 !== 0;
+
+            console.log(odd)
+              return <Card post={item} key={item.id} hasImage={odd} />
+          })
+        }
+      </div>
+      <Footer />
     </div>
   );
 }
