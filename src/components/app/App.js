95c32959c2acd5914afab7b112f0ae06c8574e62
Sophie Pankevych <sofiapankevych11@gmail.com>
2020-05-20 21:40:50 +0300

    task1 is done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
+
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
+import {Footer} from '../footer/Footer'
+
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
+import {usersList} from '../../constants'
+
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import {UserCard} from '../user-card/UserCard'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -16,7 +23,10 @@ function App() {
       <div>content will appear here soon</div>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
 
+
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
+      <UserCard user={usersList[0]} />
+      <Footer />
     </div>
   );
 }
