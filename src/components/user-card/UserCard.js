1ce42d11a2587f49b2b84886071a503799ef519e
KichuraMaksym <kichuramaksym@gmail.com>
2020-05-20 10:25:35 +0300

    1 and 2 task

--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -22,7 +22,7 @@ export const UserCard = (props) => {
     if (!user) return null;
     const {first_name, last_name, email, address, _links} =user;
     return <div className={"may-user-card card"}>
-        <img src={_links.avatar.href} className={"may-user-card-avatar rounded-circle"}/>
+        <img src={_links.avatar.href} className={"may-user-card-avatar rounded-circle"} alt={'img'}/>
         <div className={'card-body'}>
             <h4 className={'card-title'}>
                 <span>{first_name}</span>
