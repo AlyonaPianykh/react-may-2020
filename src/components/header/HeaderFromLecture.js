2107317cda6a1d4e33d68273d27e70501ed7bef1
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-05-21 02:57:55 +0300

    Task 3 is done

--- src/components/header/HeaderFromLecture.js
+++ src/components/header/HeaderFromLecture.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { links, user } from '../../constants';
 import Logo from '../../assets/react.png';
-
 import { UserInfo } from '../user-info/UserInfoFromLecture';
-
 import './Header.scss';
 
+const Link = () => {
+    return links.map(({url,name}, index) => {
+        return (
+            <div key={index} className={"nav-item"}>
+                <a key={index} href={url} className={"may-header-links-wrapper-link nav-link"}>{name}</a>
+            </div>
+        )
+    })
+};
+
 export const Header = (props) => {
-  console.log(links);
+  // console.log(links);
   return (
     <div className="may-header navbar">
-      <img src={Logo} className="may-header-logo" />
+        <img src={Logo} className="may-header-logo"  alt={'logo'}/>
 
-      <div className="may-header-links-wrapper">
-        {/* todo: переписать рендеринг этих ссылок используя links.map метод */}
-        <div className="nav-item">
-          <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
+        <div className="may-header-links-wrapper">
+            {/* todo: переписать рендеринг этих ссылок используя links.map метод */}
+            <Link/>
         </div>
-        <div className="nav-item">
-          <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
-      </div>
 
-      <UserInfo user={user} />
+        <UserInfo user={user} />
     </div>
   );
 };
