2dfa2f68a9f7736c4fad0339ffa1993e396d7886
Sanya911s <sanity2427@gmail.com>
2020-05-20 08:43:13 +0300

    - made and add Footer.js - made and add UserCard.js - add some styles for UserCard.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,32 @@
 import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
-import {socialMediaIcons, user} from "../../constants";
+
 
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
-import {Footer} from '../footer/Footer';
+import { Footer } from '../footer/Footer'
+
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
-import {usersList} from "../../constants";
+import { usersList } from "../../constants";
+
 
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import { UserCard } from '../user-card/UserCard'
+
 
 
 import './App.css';
 
-const Footer1 = (props) => {
+function App() {
     return (
-        <div>
-        footer
+        <div className="App">
+            <Header/>
+
+            <UserCard user={usersList[1]} />
+
+            <Footer/>
         </div>
     )
-};
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-        <Footer />
-       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
-    </div>
-  )
-};
+}
 export default App;
--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //todo здесь нужно сделать импорт стилей из './UserCard.scss'
-
+import './UserCard.scss'
 
 // todo здесь нужно сделать экспорт функии под названием UserCard с аргументом props
 //  нужно деструктуризировать объект props и достать из него переменную user
@@ -15,4 +15,25 @@ import React from 'react';
 //  этот div будет содержать:
 //  - блок h4 с классом card-title, он должен вывести имя и фамилию (first_name, last_name)
 //  - блок div c классом "card-text", содержащий 2 div блока для поля email и поля address
+export const UserCard = (props) => {
+    const {user} = props;
+    if (!user) return null;
+    const {first_name, last_name, email, address, _links} = user;
+    return (
+        <div className='may-user-card'
+             style={{float: 'left', border: '1px solid black', marginTop: '20px', marginLeft: '20px'}}>
+            <img className='may-user-card-avatar rounded-circle' src={_links.avatar.href} alt="avatar" style={{
+                marginLeft: '20px',
+                height: '70px'
+            }}/>
+            <div className='card-body'>
+                <h4 className='card-title'>{first_name} {last_name}</h4>
+                <div className='card-text'>
+                    <div>{email}</div>
+                    <div>{address}</div>
+                </div>
+            </div>
+        </div>
+    )
+};
 
