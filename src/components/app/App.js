e783aea03df4f238ec48bc92d4ad185b2bd88266
tarasrudkiv <tarasrudkiv@ukr.net>
2020-05-20 19:56:38 +0300

    done homework 1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
+import Footer from "../footer/Footer";
+import {usersList} from '../../constants';
+import {UserCard} from '../user-card/UserCard';
+
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
@@ -10,15 +14,23 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div>content will appear here soon</div>
-      {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
+//       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
+//       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
 
-      {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div>content will appear here soon</div>
+            <Footer/>
+            <div className="card-group">
+                {usersList.map( value => (
+                    <UserCard user={value}
+                    />
+                ))}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
