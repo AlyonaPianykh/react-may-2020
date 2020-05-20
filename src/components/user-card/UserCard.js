b56fd0c95a771966858f85ae20e57a5562eebf65
RostyslavKoltso <Rosryslav22>
2020-05-20 19:11:34 +0300

    done hw1 Koltso Rostyslav

--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 //todo здесь нужно сделать импорт стилей из './UserCard.scss'
+import './UserCard.scss';
+// import {user} from "../../constants";
 
+export  const UserCard = ({user}) => {
+    if (!user) return null;
+    const {first_name, last_name, email, address, _links: {avatar: {href}}} = user;
+    return (
+        <div className='may-user-card card' style={{float: "left"}}>
+            <img className='may-user-card-avatar rounded-circle' src={href} alt="avatar"/>
+            <div className='card-body'>
+                <h4 className='card-title'>{first_name} {last_name}</h4>
+                <div className='card-text'>
+                    <div>{email}</div>
+                    <div>{address}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 // todo здесь нужно сделать экспорт функии под названием UserCard с аргументом props
 //  нужно деструктуризировать объект props и достать из него переменную user
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App/>,
   document.getElementById('root')
 );
 
