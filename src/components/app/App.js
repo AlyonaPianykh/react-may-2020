59ee2a4413f91385a2d4c4c0437d1165b70a7b87
orangeRabit <pulyak.a.v@gmail.com>
2020-05-20 15:35:47 +0300

    task1 done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,7 +5,9 @@ import { Header } from '../header/HeaderFromLecture';
 import {Footer} from "../footer/Footer";
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
+import {usersList} from "../../constants";
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import {UserCard} from '../user-card/UserCard'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,7 +18,7 @@ function App() {
       <Header />
       <div>content will appear here soon</div>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-
+        <UserCard user={usersList[0]}/>
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
       <Footer />
     </div>
--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -4,9 +4,7 @@ import React from 'react';
 import './UserCard.scss'
 
 // todo здесь нужно сделать экспорт функии под названием UserCard с аргументом props
-export const UserCard= (props) => {
 
-}
 //  нужно деструктуризировать объект props и достать из него переменную user
 //  детальнее про деструктуризацию тут: https://learn.javascript.ru/destructuring#destrukturizatsiya-obekta
 //  сделать проверку если !user (т.е. он или undefined или null или путая строка или 0), то вернуть null
@@ -18,3 +16,23 @@ export const UserCard= (props) => {
 //  этот div будет содержать:
 //  - блок h4 с классом card-title, он должен вывести имя и фамилию (first_name, last_name)
 //  - блок div c классом "card-text", содержащий 2 div блока для поля email и поля address
+
+export const UserCard= (props) => {
+    const {user} = props;
+
+    if (!user) return null;
+    const {first_name, last_name, email, address, _links} = user;
+
+    return (
+        <div className="may-user-card card">
+            <img className="may-user-card-avatar rounded-circle" src={_links.avatar.href} alt="avatar"/>
+            <div className="card-body">
+                <h4 className="card-title">{first_name} {last_name}</h4>
+                <div className="card-text">
+                    <div>{email}</div>
+                    <div>{address}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
