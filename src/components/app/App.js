dab091276bcada331ee5f1329f17c339d3117434
sviatiq <sviatiqit@gmail.com>
2020-05-21 21:35:46 +0300

    #1 Task 1 and Task 2 completed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import { Header } from '../header/HeaderFromLecture';
+import {Header} from '../header/HeaderFromLecture';
+import {Footer} from '../footer/Footer';
+import {PostCard} from "../post-card/PostCard";
+import {UserCard} from "../user-card/UserCard";
+import {postsList} from "../../constants";
+import {usersList} from "../../constants";
 
-// todo: здесь нужно сделать импорт Footer из '../footer/Footer'
-
-// todo: здесь нужно сделать импорт usersList из '../../constants'
-// todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
-
+import './App.scss'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div>content will appear here soon</div>
-      {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
 
-      {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <div>content will appear here soon</div>
+            {/*<div className="d-flex posts-container">*/}
+            {/*    <PostCard post={postsList[0]}/>*/}
+            {/*    <PostCard post={postsList[1]}/>*/}
+            {/*    <PostCard post={postsList[2]}/>*/}
+            {/*    <PostCard post={postsList[3]}/>*/}
+            {/*    <PostCard post={postsList[4]}/>*/}
+            {/*</div>*/}
+            <Footer/>
+
+            <div className="d-flex posts-container">
+            <UserCard user = {usersList[0]} />
+            <UserCard user = {usersList[1]} />
+            <UserCard user = {usersList[2]} />
+            <UserCard user = {usersList[3]} />
+            <UserCard user = {usersList[4]} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
