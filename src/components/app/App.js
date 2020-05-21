2800b0ed0de84e204ecdb3d68fc5dc2cbc4c796b
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2020-05-21 17:01:36 +0300

    done hw2

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,26 +2,27 @@ import React from 'react';
 
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
-import TestCard, { PostCard as Card } from '../post-card/PostCard';
-import { postsList } from '../../constants'; // todo помимо константы postsList достать еще usersList
+import {postsList, usersList} from '../../constants'; // todo помимо константы postsList достать еще usersList
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
+import {UserCard} from "../user-card/UserCard";
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+// function App() {
+//
+//   const renderList = () => {
+//     const res = [];
+//
+//     for (let i = 0; i < 6; i++) {
+//       const item = postsList[i];
+//       res.push(<Card post={item} key={item.id}/>)
+//     }
+//
+//     return res;
+//   };
 function App() {
-
-  const renderList = () => {
-    const res = [];
-
-    for (let i = 0; i < 6; i++) {
-      const item = postsList[i];
-      res.push(<Card post={item} key={item.id}/>)
-    }
-
-    return res;
-  };
-
   return (
     <div className="App">
       <Header />
@@ -33,19 +34,27 @@ function App() {
       {/*  }*/}
       {/*</div>*/}
 
-      <div className="d-flex posts-container">
-        {
-          postsList.map((item, index) => {
-            const odd = index % 2 !== 0;
+      {/*<div className="d-flex posts-container">*/}
+      {/*  {*/}
+      {/*    postsList.map((item, index) => {*/}
+      {/*      const odd = index % 2 !== 0;*/}
 
-            console.log(odd)
-              return <Card post={item} key={item.id} hasImage={odd} />
-          })
-        }
-      </div>
+      {/*      console.log(odd)*/}
+      {/*        return <Card post={item} key={item.id} hasImage={odd} />*/}
+      {/*    })*/}
+      {/*  }*/}
+      {/*</div>*/}
 
       <div className="d-flex posts-container">
       {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+       {
+              usersList.map((item)=>{
+                  return(
+                      <UserCard user={item} key={item.id} />
+                  )
+              })
+          }
+
       </div>
 
       <Footer />
@@ -54,3 +63,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/header/HeaderFromLecture.js
+++ src/components/header/HeaderFromLecture.js
@@ -14,15 +14,24 @@ export const Header = (props) => {
 
       <div className="may-header-links-wrapper">
         {/* todo: переписать рендеринг этих ссылок используя links.map метод */}
-        <div className="nav-item">
-          <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
+         {
+              links.map((link, id) => {
+                  return (
+                      <div key={id} className="nav-item">
+                          <a href={link.url} className="may-header-links-wrapper-link nav-link">{link.name}</a>
+                      </div>
+                  )
+              })
+          }
+      {/*  <div className="nav-item">*/}
+      {/*    <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>*/}
+      {/*  </div>*/}
+      {/*  <div className="nav-item">*/}
+      {/*    <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>*/}
+      {/*  </div>*/}
+      {/*  <div className="nav-item">*/}
+      {/*    <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>*/}
+      {/*  </div>*/}
       </div>
 
       <UserInfo user={user} />
