2f043b998dfa19b9ec666f1d250e5146b45bc14b
bohdanbolyubash <bbolyubash@icloud.com>
2020-05-21 16:24:22 +0300

    - i created new renders

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
-import { postsList } from '../../constants'; // todo помимо константы postsList достать еще usersList
+import { postsList, usersList } from '../../constants';
+// todo помимо константы postsList достать еще usersList
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
+import {UserCard} from './../user-card/UserCard'
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -37,17 +39,20 @@ function App() {
         {
           postsList.map((item, index) => {
             const odd = index % 2 !== 0;
-
-            console.log(odd)
-              return <Card post={item} key={item.id} hasImage={odd} />
+                if(index < 8)
+            return <Card post={item} key={item.id} hasImage={odd} />
           })
         }
       </div>
 
-      <div className="d-flex posts-container">
-      {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
-      </div>
+      <div className="d-flex posts-container">{/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
 
+          {
+              usersList.map((item,index)=>{
+                  return <UserCard user={item}/>
+              })
+          }
+      </div>
       <Footer />
     </div>
   );
--- src/components/header/HeaderFromLecture.js
+++ src/components/header/HeaderFromLecture.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { links, user } from '../../constants';
+import {links, postsList, user} from '../../constants';
 import Logo from '../../assets/react.png';
 
 import { UserInfo } from '../user-info/UserInfoFromLecture';
 
 import './Header.scss';
+import {PostCard as Card} from "../post-card/PostCard";
 
 export const Header = (props) => {
   console.log(links);
@@ -12,17 +13,25 @@ export const Header = (props) => {
     <div className="may-header navbar">
       <img src={Logo} className="may-header-logo" />
 
-      <div className="may-header-links-wrapper">
+      <div className=" may-header-links-wrapper">
         {/* todo: переписать рендеринг этих ссылок используя links.map метод */}
-        <div className="nav-item">
-          <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
+          <div className="d-flex nav-item">
+          {
+              links.map((item, index)=>{
+                 return <a href={item.url} className="may-header-links-wrapper-link nav-link" key={index}> {item.name}</a>
+              })
+          }
+          </div>
+
+        {/*<div className="nav-item">*/}
+        {/*  <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>*/}
+        {/*</div>*/}
+        {/*<div className="nav-item">*/}
+        {/*  <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>*/}
+        {/*</div>*/}
+        {/*<div className="nav-item">*/}
+        {/*  <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>*/}
+        {/*</div>*/}
       </div>
 
       <UserInfo user={user} />
