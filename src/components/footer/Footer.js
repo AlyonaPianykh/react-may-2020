4b1e098d84c42675625aba08cdbb335dd4bd2ce9
KichuraMaksym <kichuramaksym@gmail.com>
2020-05-21 14:27:09 +0300

    footer and header refactor

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -18,15 +18,18 @@ import './Footer.scss';
 
 export const Footer = () => {
     return <div className={'may-footer'}>
-            <div className={'may-footer-content'}>
-                <div>"Some text about authors"</div>
-                <div className={'may-footer-social-media-wrapper'}>
-                    <img src={socialMediaIcons[0].src} className='may-footer-social-media-icon' alt={socialMediaIcons[0].alt}/>
-                    <img src={socialMediaIcons[1].src} className='may-footer-social-media-icon' alt={socialMediaIcons[1].alt}/>
-                    <img src={socialMediaIcons[2].src} className='may-footer-social-media-icon' alt={socialMediaIcons[2].alt}/>
-                </div>
+        <div className={'may-footer-content'}>
+            <div>"Some text about authors"</div>
+            <div className={'may-footer-social-media-wrapper'}>
+                {/*    <img src={socialMediaIcons[0].src} className='may-footer-social-media-icon' alt={socialMediaIcons[0].alt}/>*/}
+                {/*    <img src={socialMediaIcons[1].src} className='may-footer-social-media-icon' alt={socialMediaIcons[1].alt}/>*/}
+                {/*    <img src={socialMediaIcons[2].src} className='may-footer-social-media-icon' alt={socialMediaIcons[2].alt}/>*/}
+                {socialMediaIcons.map((value, index) => {
+                    return <img key={index} src={value.src} alt={value.alt} className='may-footer-social-media-icon'/>
+                })}
             </div>
-            <div>'All rights reserved. may-app © 2020'</div>
-        </div>;
+        </div>
+        <div>'All rights reserved. may-app © 2020'</div>
+    </div>;
 }
 
--- src/components/header/HeaderFromLecture.js
+++ src/components/header/HeaderFromLecture.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { links, user } from '../../constants';
+import {links, user} from '../../constants';
 import Logo from '../../assets/react.png';
 
-import { UserInfo } from '../user-info/UserInfoFromLecture';
+import {UserInfo} from '../user-info/UserInfoFromLecture';
 
 import './Header.scss';
 
 export const Header = (props) => {
-  console.log(links);
-  return (
-    <div className="may-header navbar">
-      <img src={Logo} className="may-header-logo" />
+    console.log(links);
+    return (
+        <div className="may-header navbar">
+            <img src={Logo} className="may-header-logo"/>
 
-      <div className="may-header-links-wrapper">
-        <div className="nav-item">
-          <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
+            {/*<div className="may-header-links-wrapper">*/}
+            {/*  <div className="nav-item">*/}
+            {/*    <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>*/}
+            {/*  </div>*/}
+            {/*  <div className="nav-item">*/}
+            {/*    <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>*/}
+            {/*  </div>*/}
+            {/*  <div className="nav-item">*/}
+            {/*    <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+            {links.map((value, index) => {
+                return <a key={index} href={value.url} className={"may-header-links-wrapper"}>{value.name}</a>
+            })}
+            <UserInfo user={user}/>
         </div>
-        <div className="nav-item">
-          <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
-      </div>
-
-      <UserInfo user={user} />
-    </div>
-  );
+    );
 };
