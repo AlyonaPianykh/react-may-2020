0dd705dead72c2c214f4f2548a9015cd8855f9c3
PashaShevchuk <pasha608@gmail.com>
2020-05-21 16:56:52 +0300

    - completed task-3 and task-4 - doing task-5

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import {Header} from '../header/HeaderFromLecture';
 import {Footer} from '../footer/Footer';
-import {PostCard} from '../post-card/PostCard';
-import {postsList, usersList} from '../../constants'; // todo помимо константы postsList достать еще usersList
+import {PostCard as Card} from '../post-card/PostCard';
+
+// todo помимо константы postsList достать еще usersList
+// todo: достать массив allComments из констант
+import {postsList, usersList, allComments} from '../../constants';
+
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
 import {UserCard} from "../user-card/UserCard";
 
@@ -19,10 +23,31 @@ function App() {
                 {
                     postsList.map((item, index) => {
                         const odd = index % 2 !== 0;
-                        return <PostCard key={item.id} post={item} hasImage={odd}/>
+                        // todo: найти в массиве usersList пользователя, айди которого равно user_id в посте (т.е. в данном случае item)
+                        //  для этого можно использовать функцию массива find или findIndex
+                        //  передать имя и фамилию пользователя как пропсу author в Card
+                        //  использовать для этого стринговый литерал ``
+                        //  в Card под телом поста срендерить имя автора, используя blockquote-footer класс из бутстрапа
+                        //  пример тут: https://hackerthemes.com/bootstrap-cheatsheet/#blockquote-footer
+                        const usersComments = usersList.find(user => {
+                            return user.id === item.user_id
+                        })
+
+
+                        // todo: найти в массиве allComments комментарии, post_id которых = id поста  (т.е. в данном случае item.id)
+                        //  для этого можно использовать метод массива filter
+                        //  передать этот массив в Card как пропсу под названиес comments
+                        const commentsPosts = allComments.filter(comment => {
+                            return comment.post_id === item.id
+                        })
+
+                        return <Card post={item} key={item.id} hasImage={odd}
+                                     author={`${usersComments.first_name} ${usersComments.last_name}`}
+                                     comments={commentsPosts}/>
                     })
                 }
             </div>
+
             {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
             <div className="d-flex posts-container">
                 {
@@ -34,58 +59,6 @@ function App() {
             <Footer/>
         </div>
     );
-
-  const renderList = () => {
-    const res = [];
-
-    for (let i = 0; i < 6; i++) {
-      const item = postsList[i];
-      res.push(<Card post={item} key={item.id}/>)
-    }
-
-    return res;
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      <div>content will appear here soon</div>
-
-      {/*<div className="d-flex posts-container">*/}
-      {/*  {*/}
-      {/*    renderList()*/}
-      {/*  }*/}
-      {/*</div>*/}
-
-      <div className="d-flex posts-container">
-        {
-          postsList.map((item, index) => {
-            const odd = index % 2 !== 0;
-            // todo: найти в массиве usersList пользователя, айди которого равно user_id в посте (т.е. в данном случае item)
-            //  для этого можно использовать функцию массива find или findIndex
-            //  передать имя и фамилию пользователя как пропсу author в Card
-            //  использовать для этого стринговый литерал ``
-            //  в Card под телом поста срендерить имя автора, используя blockquote-footer класс из бутстрапа
-            //  пример тут: https://hackerthemes.com/bootstrap-cheatsheet/#blockquote-footer
-
-
-            // todo: найти в массиве allComments комментарии, post_id которых = id поста  (т.е. в данном случае item.id)
-            //  для этого можно использовать метод массива filter
-            //  передать этот массив в Card как пропсу под названиес comments
-
-              return <Card post={item} key={item.id} hasImage={odd} />
-          })
-        }
-      </div>
-
-      <div className="d-flex posts-container">
-      {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
-
-      </div>
-
-      <Footer />
-    </div>
-  );
 }
 
 export default App;
--- src/components/post-card/PostCard.js
+++ src/components/post-card/PostCard.js
@@ -1,75 +1,64 @@
 import React from 'react';
-
 import DefaultImg from '../../assets/default-empty-img.png';
+
 import './PostCard.scss';
 
 export function PostCard(props) {
-  console.log(props);
-  // todo: достать пропсу comments из props ниже в 9й строке
-  const { post, hasImage } = props;
+    // todo: достать пропсу comments из props ниже в 9й строке
+    const {post, hasImage, comments, author} = props;
+    const {title, body} = post;
+    const kittyUrl = `https://cataas.com/cat/says/hello%20world!?${Math.random() * 1000}`;
 
-  console.log('hasImage', hasImage);
-  const { title, body } = post;
+    return (
+        <div className="may-post-card card">
+            {hasImage && (
+                <div className="may-post-card-img" id="my-block" onClick={() => {
+                    alert('this post has a picture')
+                }}>
+                    <img src={kittyUrl}/>
+                </div>
+            )
+            }
+            {!hasImage && (
+                <div className="may-post-card-img">
+                    <img src={DefaultImg}/>
+                </div>
+            )}
 
-  const kittyUrl = `https://cataas.com/cat/says/hello%20world!?${Math.random() * 1000}`;
+            <div className="card-body">
+                <h4 className="card-title title">{title}</h4>
+                <div className="card-text body">
+                    {body}
+                </div>
+            </div>
 
-  const renderImage = () => {
-    return hasImage ? (
-      <img src={kittyUrl} />
-    ) : (
-      <img src={DefaultImg} />
-    );
-  };
+            <footer className="blockquote-footer">Author's name:
+                <cite title="Source Title">  {author}</cite>
+            </footer>
 
-  return (
-    <div className="may-post-card card">
-
-      {/*<div className="may-post-card-img">*/}
-      {/*  {*/}
-      {/*    hasImage ? (*/}
-      {/*      <img src={kittyUrl} />*/}
-      {/*      ) : (*/}
-      {/*        <img src={DefaultImg} />*/}
-      {/*    )*/}
-      {/*  }*/}
-      {/*</div>*/}
-
-      {/*<div className="may-post-card-img" >*/}
-      {/*  {*/}
-      {/*    renderImage()*/}
-      {/*  }*/}
-      {/*</div>*/}
-
-      {hasImage && (
-        <div className="may-post-card-img" id="my-block" onClick={() => {alert('ghvcdhfvbdfsjvbdf')}}>
-          <img src={kittyUrl} />
-        </div>
-      )
-      }
-      {!hasImage && (
-        <div className="may-post-card-img">
-          <img src={DefaultImg} />
-        </div>
-      )}
-
-      <div className="card-body">
-        <h4 className="card-title title">{title}</h4>
-        <div className="card-text body">
-          {body}
-        </div>
-      </div>
-
-    {/*  todo: здесь нужно показать массив коментариев к посту (comments), который прилетит в props
+            {/* todo: здесь нужно показать массив коментариев к посту (comments), который прилетит в props
           можно создать отдельную компоненту Comment по аналогии с тем, как мы делали PostCard, UserCard
           как она будет выглядеть зависит от вашей фантазии
           Для каждого комментария должны быть выведены такие поля:
           name - имя автора комментария
           email - почта автора комментария
-          body - текст комментария
-    */}
-
-    </div>
-  );
+          body - текст комментария  */}
+            {
+                comments.map(item => {
+                    return (
+                        <div className='post-comment'>
+                            <div>Name: {item.name}</div>
+                            <div>email: {item.email}</div>
+                            <div className='post-comment-body'>
+                                <span>Comment: </span>
+                                <p>{item.body}</p>
+                            </div>
+                        </div>
+                    );
+                })
+            }
+        </div>
+    );
 }
 
 export default PostCard;
