a0b178ae0327d77fdf6e4b652234521e85986579
Ostap121 <ostappavlentiy@gmail.com>
2020-05-21 12:42:13 +0300

    commit one

--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 //todo здесь нужно сделать импорт стилей из './UserCard.scss'
+import './UserCard.scss'
 
-
+export const UserCard = (props) =>{
+    const {user} = props;
+    if (!user) return null;
+    const {first_name, last_name, email, address, _links:{avatar:{href}}} = user;
+    return(
+        <div className='may-user-card card'>
+            <img src={href} className='may-user-card-avatar rounded-circle'/>
+            <div className='card-body'>
+                <h4 className='card-title'>
+                    {first_name} {last_name}
+                </h4>
+                <div className='card-text'>
+                    <div>
+                        {email}
+                    </div>
+                    <div>
+                        {address}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
 // todo здесь нужно сделать экспорт функии под названием UserCard с аргументом props
 //  нужно деструктуризировать объект props и достать из него переменную user
 //  детальнее про деструктуризацию тут: https://learn.javascript.ru/destructuring#destrukturizatsiya-obekta
