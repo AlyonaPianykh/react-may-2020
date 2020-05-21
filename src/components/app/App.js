d1a1c7017ff6c0902b64879070c12f2402a6bb8c
VovaKrychkivskyi <4Fa263izmnSurDnTa8fY>
2020-05-21 11:55:53 +0300

    Add task#1 (User-Card)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 import { Footer} from "../footer/Footer";
-
-// todo: здесь нужно сделать импорт usersList из '../../constants'
-// todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
-
+import { usersList } from '../../constants';
+import { UserCard } from '../user-card/UserCard';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,9 +11,8 @@ function App() {
     <div className="App">
       <Header />
       <div>content will appear here soon</div>
+        <UserCard user ={usersList[0]}/>
       <Footer/>
-
-      {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
     </div>
   );
 }
