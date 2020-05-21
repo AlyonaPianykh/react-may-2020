02090f5f5187961a385d1296c412f58137e54f8a
xeilen <xeilen1112@gmail.com>
2020-05-21 18:02:19 +0300

    -HomeWork 2 status: done task 3 and 4

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import { socialMediaIcons } from '../../constants';
+import {socialMediaIcons} from '../../constants';
 
 import './Footer.scss';
 
 export function Footer(props) {
-  return (
-    <div className="may-footer">
-      <div className="may-footer-content">
-      <div className="may-footer-text">Some text about authors</div>
-      <div className="may-footer-social-media-wrapper">
-          {socialMediaIcons.map((value, index) =>
-              <img key={index} className="may-footer-social-media-icon" src={value.src} alt={value.alt}/>)}
-      </div>
-      </div>
-      <div>All rights reserved. may-app © 2020</div>
-    </div>
-  );
+    return (
+        <div className="may-footer">
+            <div className="may-footer-content">
+                <div className="may-footer-text">Some text about authors</div>
+                <div className="may-footer-social-media-wrapper">
+                    {socialMediaIcons.map((value, index) =>
+                        <img key={index} className="may-footer-social-media-icon" src={value.src} alt={value.alt}/>)}
+                </div>
+            </div>
+            <div>All rights reserved. may-app © 2020</div>
+        </div>
+    );
 }
 
