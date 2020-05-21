6bea49649fad680913ed00e3dab982fd97886480
bohdanbolyubash <bbolyubash@icloud.com>
2020-05-21 17:22:01 +0300

    - i created refactors

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
-import { postsList } from '../../constants'; // todo помимо константы postsList достать еще usersList
+import { postsList, usersList } from '../../constants';
+ // todo помимо константы postsList достать еще usersList
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
+import {UserCard} from './../user-card/UserCard'
+
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -45,7 +48,12 @@ function App() {
       </div>
 
       <div className="d-flex posts-container">
-      {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+          {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+              {
+                  usersList.map((item,index)=>{
+                      return <UserCard user={item}/>
+                  })
+              }
       </div>
 
       <Footer />
