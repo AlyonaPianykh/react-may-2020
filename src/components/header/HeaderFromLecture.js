e0b8a704e27faef88fd337cfb5d9273a541d38d4
Sanya911s <sanity2427@gmail.com>
2020-05-22 14:12:14 +0300

    - make task 3 and 4

--- src/components/header/HeaderFromLecture.js
+++ src/components/header/HeaderFromLecture.js
@@ -7,22 +7,22 @@ import { UserInfo } from '../user-info/UserInfoFromLecture';
 import './Header.scss';
 
 export const Header = (props) => {
-  console.log(links);
+  console.log(props);
   return (
     <div className="may-header navbar">
-      <img src={Logo} className="may-header-logo" />
+      <img src={Logo} className="may-header-logo" alt='logo'/>
 
       <div className="may-header-links-wrapper">
         {/* todo: переписать рендеринг этих ссылок используя links.map метод */}
-        <div className="nav-item">
-          <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
+          {
+              links.map(((value) => {
+                  return (
+                      <div className="nav-item" key={value.name}>
+                          <a href={value.url} className="may-header-links-wrapper-link nav-link">{value.name}</a>
+                      </div>
+                  )
+              }))
+          }
       </div>
 
       <UserInfo user={user} />
--- src/components/post-card/PostCard.js
+++ src/components/post-card/PostCard.js
@@ -5,50 +5,21 @@ import './PostCard.scss';
 
 export function PostCard(props) {
   console.log(props);
-
   const { post, hasImage } = props;
-
   console.log('hasImage', hasImage);
   const { title, body } = post;
-
   const kittyUrl = `https://cataas.com/cat/says/hello%20world!?${Math.random() * 1000}`;
-
-  const renderImage = () => {
-    return hasImage ? (
-      <img src={kittyUrl} />
-    ) : (
-      <img src={DefaultImg} />
-    );
-  };
-
   return (
     <div className="may-post-card card">
-
-      {/*<div className="may-post-card-img">*/}
-      {/*  {*/}
-      {/*    hasImage ? (*/}
-      {/*      <img src={kittyUrl} />*/}
-      {/*      ) : (*/}
-      {/*        <img src={DefaultImg} />*/}
-      {/*    )*/}
-      {/*  }*/}
-      {/*</div>*/}
-
-      {/*<div className="may-post-card-img" >*/}
-      {/*  {*/}
-      {/*    renderImage()*/}
-      {/*  }*/}
-      {/*</div>*/}
-
       {hasImage && (
         <div className="may-post-card-img" id="my-block" onClick={() => {alert('ghvcdhfvbdfsjvbdf')}}>
-          <img src={kittyUrl} />
+          <img src={kittyUrl} alt='kitty'/>
         </div>
       )
       }
       {!hasImage && (
         <div className="may-post-card-img">
-          <img src={DefaultImg} />
+          <img src={DefaultImg} alt='defaultImg'/>
         </div>
       )}
 
