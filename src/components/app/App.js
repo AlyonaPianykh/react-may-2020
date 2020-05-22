2755973b138976bf136c658c5451adddd38223fa
yaroslavhnatysko <gnatysko96@gmail.com>
2020-05-22 17:54:51 +0300

    Task 1 Done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
-// todo: здесь нужно сделать импорт Footer из '../footer/Footer'
+import {Footer} from "../footer/Footer";
+import {usersList} from '../../constants'
+import {UserCard} from '../user-card/UserCard'
 
+// todo: здесь нужно сделать импорт Footer из '../footer/Footer'ddddd
 // todo: здесь нужно сделать импорт usersList из '../../constants'
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
 
@@ -13,9 +16,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div>content will appear here soon</div>
-      {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
 
+        <div>content will appear here soon</div>
+        <UserCard user={usersList[0]}/>
+      <Footer />
+
+        {/*todo: здесь нужно использовать Footer по аналогии с Header */}
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
     </div>
   );
