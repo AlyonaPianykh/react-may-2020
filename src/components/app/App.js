967b22c58cd251104565cbe8682c3e4b61da18b2
EshkerePro <mikola0672662963@gmail.com>
2020-05-22 18:22:27 +0300

    Homework1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
+import { Footer } from '../footer/Footer';
+import { UserCard } from "../user-card/UserCard";
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import  {usersList} from "../../constants";
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,8 +18,16 @@ function App() {
       <Header />
       <div>content will appear here soon</div>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-
+        {
+            usersList.map(value => {
+                return(
+                    <UserCard user={value} />
+                )
+            })
+        }
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
+
+      <Footer />
     </div>
   );
 }
