9f27c0d3239f09b6549770ea6b8a51964141830d
SergGrishkov <serg.grishkov@gmail.com>
2020-05-23 22:30:08 +0300

    Test commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React TEST
         </a>
       </header>
     </div>
