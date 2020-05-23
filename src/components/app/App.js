b4031009650f8da45ce18b3eeb761f02eb05cdde
VovaKrychkivskyi <4Fa263izmnSurDnTa8fY>
2020-05-23 09:05:16 +0300

    Add task#2 (task#3,task#4)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Header } from '../header/HeaderFromLecture';
-import { Footer } from '../footer/Footer';
-import TestCard, { PostCard as Card } from '../post-card/PostCard';
-import { postsList, usersList } from '../../constants'; //
+import {Header} from '../header/HeaderFromLecture';
+import {Footer} from '../footer/Footer';
+import TestCard, {PostCard as Card} from '../post-card/PostCard';
+import {postsList, usersList} from '../../constants'; //
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
 import {UserCard} from "../user-card/UserCard";
 import './App.scss';
@@ -11,51 +11,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const renderList = () => {
-    const res = [];
+    const renderList = () => {
+        const res = [];
 
-    for (let i = 0; i < 6; i++) {
-      const item = postsList[i];
-      res.push(<Card post={item} key={item.id}/>)
-    }
-
-    return res;
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      <div>content will appear here soon</div>
-
-      {/*<div className="d-flex posts-container">*/}
-      {/*  {*/}
-      {/*    renderList()*/}
-      {/*  }*/}
-      {/*</div>*/}
-
-      <div className="d-flex posts-container">
-        {
-          postsList.map((item, index) => {
-            const odd = index % 2 !== 0;
-
-            console.log(odd);
-              return <Card post={item} key={item.id} hasImage={odd} />
-          })
+        for (let i = 0; i < 6; i++) {
+            const item = postsList[i];
+            res.push(<Card post={item} key={item.id}/>)
         }
-      </div>
 
-      <div className="d-flex posts-container">
-      {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
-          {usersList.map((value) => {
-              return<
-                  UserCard user={value} key={value.id}
-              />
-          })}
-      </div>
+        return res;
+    };
 
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div>content will appear here soon</div>
+
+            {/*<div className="d-flex posts-container">*/}
+            {/*  {*/}
+            {/*    renderList()*/}
+            {/*  }*/}
+            {/*</div>*/}
+
+            <div className="d-flex posts-container">
+                {
+                    postsList.map((item, index) => {
+                        const odd = index % 2 !== 0;
+
+                        console.log(odd);
+                        return <Card post={item} key={item.id} hasImage={odd}/>
+                    })
+                }
+            </div>
+
+            <div className="d-flex posts-container">
+                {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+                {usersList.map((value) => {
+                    return <
+                        UserCard user={value} key={value.id}
+                    />
+                })}
+            </div>
+
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,27 +1,28 @@
 import React from 'react';
 
-import { socialMediaIcons } from '../../constants';
+import {socialMediaIcons} from '../../constants';
 
 import './Footer.scss';
 
 export function Footer(props) {
-  return (
-    <div className="may-footer">
-      <div className="may-footer-content">
-      <div className="may-footer-text">Some text about authors</div>
-      <div className="may-footer-social-media-wrapper">
-          {socialMediaIcons.map((value, index) => {
-              return(
-                  <a key={index} href={value.src}>{value.alt}</a>
-              )
-          })}
-        {/*<img className="may-footer-social-media-icon" src={socialMediaIcons[0].src} alt={socialMediaIcons[0].alt}/>
+    return (
+        <div className="may-footer">
+            <div className="may-footer-content">
+                <div className="may-footer-text">Some text about authors</div>
+                <div className="may-footer-social-media-wrapper">
+                    {socialMediaIcons.map((value, index) => {
+                        return (
+                            <img key={index} className="may-footer-social-media-icon" src={value.src} alt={value.alt}/>
+                            //<a key={index} href={value.src}>{value.alt}</a>
+                        )
+                    })}
+                    {/*<img className="may-footer-social-media-icon" src={socialMediaIcons[0].src} alt={socialMediaIcons[0].alt}/>
         <img className="may-footer-social-media-icon" src={socialMediaIcons[1].src} alt={socialMediaIcons[1].alt}/>
         <img className="may-footer-social-media-icon" src={socialMediaIcons[2].src} alt={socialMediaIcons[2].alt}/>*/}
-      </div>
-      </div>
-      <div>All rights reserved. may-app © 2020</div>
-    </div>
-  );
+                </div>
+            </div>
+            <div>All rights reserved. may-app © 2020</div>
+        </div>
+    );
 }
 
