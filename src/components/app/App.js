43cbb122c5d736dd2fb1f81da7b024ceb75aefbc
ArtemMachulenko <dr.a.mefesto@gmail.com>
2020-05-23 10:21:00 +0300

    -corrected extra task

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -11,9 +11,7 @@ import {UserCard} from "../user-card/UserCard";
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// console.log(allComments);
-// console.log(postsList)
-// console.log(usersList);
+
 function App() {
   const renderList = () => {
     const res = [];
@@ -40,14 +38,14 @@ function App() {
       <div className="d-flex posts-container">
         {
           postsList.map((post, index) => {
-            const odd = post % 2 !== 0;
+            const odd = index % 2 !== 0;
+              
             // todo: найти в массиве usersList пользователя, айди которого равно user_id в посте (т.е. в данном случае item)
             //  для этого можно использовать функцию массива find или findIndex
             //  передать имя и фамилию пользователя как пропсу author в Card
             //  использовать для этого стринговый литерал ``
             //  в Card под телом поста срендерить имя автора, используя blockquote-footer класс из бутстрапа
             //  пример тут: https://hackerthemes.com/bootstrap-cheatsheet/#blockquote-footer
-
               const {first_name, last_name} = usersList.find(user => user.id === post.user_id);
 
             // todo: найти в массиве allComments комментарии, post_id которых = id поста  (т.е. в данном случае item.id)
@@ -67,12 +65,12 @@ function App() {
           {
               usersList.map(user => {
                return (
-                   <UserCard user={user} />
+                   <UserCard user={user} key={user.id} />
                );
            })
           }
       </div>
-
+        
       <Footer />
     </div>
   );
