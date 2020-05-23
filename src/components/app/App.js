4a2648562a737797f3503d033c2d48aa913669ec
YevhenSel <Yevhen.Sel@gmail.com>
2020-05-23 23:10:28 +0300

    task2

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,68 +1,78 @@
 import React from 'react';
 
-import { Header } from '../header/HeaderFromLecture';
-import { Footer } from '../footer/Footer';
-import TestCard, { PostCard as Card } from '../post-card/PostCard';
-// todo: достать в строке 7 массив allComments из констант
-import { postsList } from '../../constants'; // todo помимо константы postsList достать еще usersList
-// todo: тут сделать импорт  UserCard из components/user-card/UserCard
+import {Header} from '../header/HeaderFromLecture';
+import {Footer} from '../footer/Footer';
+import TestCard, {PostCard as Card} from '../post-card/PostCard';
+// donetodo: достать в строке 7 массив allComments из констант
+import {postsList, allComments, usersList} from '../../constants';
+// donetodo помимо константы postsList достать еще usersList
+// donetodo: тут сделать импорт  UserCard из components/user-card/UserCard
+import {UserCard} from '../user-card/UserCard';
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const renderList = () => {
-    const res = [];
+	const renderList = () => {
+		const res = [];
 
-    for (let i = 0; i < 6; i++) {
-      const item = postsList[i];
-      res.push(<Card post={item} key={item.id}/>)
-    }
+		for (let i = 0; i < 6; i++) {
+			const item = postsList[i];
+			res.push(<Card post={item} key={item.id}/>)
+		}
 
-    return res;
-  };
+		return res;
+	};
 
-  return (
-    <div className="App">
-      <Header />
-      <div>content will appear here soon</div>
+	return (
+		<div className="App">
+			<Header/>
+			<div>content will appear here soon</div>
 
-      {/*<div className="d-flex posts-container">*/}
-      {/*  {*/}
-      {/*    renderList()*/}
-      {/*  }*/}
-      {/*</div>*/}
+			{/*<div className="d-flex posts-container">*/}
+			{/*  {*/}
+			{/*    renderList()*/}
+			{/*  }*/}
+			{/*</div>*/}
 
-      <div className="d-flex posts-container">
-        {
-          postsList.map((item, index) => {
-            const odd = index % 2 !== 0;
-            // todo: найти в массиве usersList пользователя, айди которого равно user_id в посте (т.е. в данном случае item)
-            //  для этого можно использовать функцию массива find или findIndex
-            //  передать имя и фамилию пользователя как пропсу author в Card
-            //  использовать для этого стринговый литерал ``
-            //  в Card под телом поста срендерить имя автора, используя blockquote-footer класс из бутстрапа
-            //  пример тут: https://hackerthemes.com/bootstrap-cheatsheet/#blockquote-footer
+			<div className="d-flex posts-container">
+				{
+					postsList.map((item, index) => {
+						const odd = index % 2 !== 0;
+						// todo: найти в массиве usersList пользователя, айди которого равно user_id в посте (т.е. в данном случае item)
+						//  для этого можно использовать функцию массива find или findIndex
+						//  передать имя и фамилию пользователя как пропсу author в Card
+						//  использовать для этого стринговый литерал ``
+						//  в Card под телом поста срендерить имя автора, используя blockquote-footer класс из бутстрапа
+						//  пример тут: https://hackerthemes.com/bootstrap-cheatsheet/#blockquote-footer
+						const user = usersList.find(user => user.id === item.id);
+						const {first_name, last_name} = user;
+						const author = `${first_name} ${last_name}`;
 
+						// todo: найти в массиве allComments комментарии, post_id которых = id поста  (т.е. в данном случае item.id)
+						//  для этого можно использовать метод массива filter
+						//  передать этот массив в Card как пропсу под названиес comments
 
-            // todo: найти в массиве allComments комментарии, post_id которых = id поста  (т.е. в данном случае item.id)
-            //  для этого можно использовать метод массива filter
-            //  передать этот массив в Card как пропсу под названиес comments
+						return <Card post={item} key={item.id} hasImage={odd}/>
+					})
+				}
+			</div>
 
-              return <Card post={item} key={item.id} hasImage={odd} />
-          })
-        }
-      </div>
+			<div className="d-flex posts-container">
+				{/*  donetodo: срендерить тут список пользователей, используя компонент UserCard */}
+				{
+					usersList.map((value) => {
+						return (
+							<UserCard key={value.id} user={value}/>
+						)
+					})
+				}
+			</div>
 
-      <div className="d-flex posts-container">
-      {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
-
-      </div>
-
-      <Footer />
-    </div>
-  );
+			<Footer/>
+		</div>
+	);
 }
 
 export default App;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-import { socialMediaIcons } from '../../constants';
+import {socialMediaIcons} from '../../constants';
 
 import './Footer.scss';
 
 export function Footer(props) {
-  return (
-    <div className="may-footer">
-      <div className="may-footer-content">
-      <div className="may-footer-text">Some text about authors</div>
-      <div className="may-footer-social-media-wrapper">
-        <img className="may-footer-social-media-icon" src={socialMediaIcons[0].src} alt={socialMediaIcons[0].alt}/>
-        <img className="may-footer-social-media-icon" src={socialMediaIcons[1].src} alt={socialMediaIcons[1].alt}/>
-        <img className="may-footer-social-media-icon" src={socialMediaIcons[2].src} alt={socialMediaIcons[2].alt}/>
-      </div>
-      </div>
-      <div>All rights reserved. may-app © 2020</div>
-    </div>
-  );
+	return (
+		<div className="may-footer">
+			<div className="may-footer-content">
+				<div className="may-footer-text">Some text about authors</div>
+				<div className="may-footer-social-media-wrapper">
+					{socialMediaIcons.map((value, index) => {
+						return (
+							<img key={index} className="may-footer-social-media-icon" src={value.src} alt={value.alt}/>
+						)
+					})}
+				</div>
+			</div>
+			<div>All rights reserved. may-app © 2020</div>
+		</div>
+	);
 }
-
