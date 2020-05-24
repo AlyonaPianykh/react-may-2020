5321525d1dda1f56af64d5f9fa034bb6058845a0
rostyslav4081 <rostuslav9913@gmail.com>
2020-05-24 13:40:57 +0300

    hw_1_SK

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
-
+import {Footer} from "../footer/Footer";
 // todo: здесь нужно сделать импорт usersList из '../../constants'
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
-
+import {usersList} from "../../constants";
+import {UserCard} from "../user-card/UserCard";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,8 +16,9 @@ function App() {
       <Header />
       <div>content will appear here soon</div>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-
+      <UserCard user={usersList[0]}/>
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
+      <Footer/>
     </div>
   );
 }
