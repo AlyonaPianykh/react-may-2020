f9f14b73ce8fa2c3103fbfead5eb1f7989290e1e
skorobahatko <maxim.skorobagatko@gmail.com>
2020-05-24 17:41:55 +0300

    added footer task + style for comments

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,11 +3,8 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
-// todo: достать в строке 7 массив allComments из констант
 import {allComments, user} from '../../constants/index';
 import {postsList, usersList} from '../../constants';
-// todo помимо константы postsList достать еще usersList
-// todo: тут сделать импорт  UserCard из components/user-card/UserCard
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -34,20 +31,13 @@ function App() {
         {
           postsList.map((item, index) => {
             const odd = index % 2 !== 0;
-            // todo: найти в массиве usersList пользователя, айди которого равно user_id в посте (т.е. в данном случае item)
-            //  для этого можно использовать функцию массива find или findIndex
-            //  передать имя и фамилию пользователя как пропсу author в Card
-            //  использовать для этого стринговый литерал ``
-            //  в Card под телом поста срендерить имя автора, используя blockquote-footer класс из бутстрапа
-            //  пример тут: https://hackerthemes.com/bootstrap-cheatsheet/#blockquote-footer
 
               let user = usersList.find(user => user.id === item.user_id);
+
               let {first_name, last_name} = user;
+
               let author = `${first_name} ${last_name}`;
 
-            // todo: найти в массиве allComments комментарии, post_id которых = id поста  (т.е. в данном случае item.id)
-            //  для этого можно использовать метод массива filter
-            //  передать этот массив в Card как пропсу под названиес comments
               let comment = allComments.filter(item => item.post_id === item.id);
               console.log(comment);
 
@@ -57,8 +47,6 @@ function App() {
       </div>
 
       <div className="d-flex posts-container">
-      {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
-
           {
               usersList.map((item, index) => {
                   return (
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,9 +10,15 @@ export function Footer(props) {
       <div className="may-footer-content">
       <div className="may-footer-text">Some text about authors</div>
       <div className="may-footer-social-media-wrapper">
-        <img className="may-footer-social-media-icon" src={socialMediaIcons[0].src} alt={socialMediaIcons[0].alt}/>
-        <img className="may-footer-social-media-icon" src={socialMediaIcons[1].src} alt={socialMediaIcons[1].alt}/>
-        <img className="may-footer-social-media-icon" src={socialMediaIcons[2].src} alt={socialMediaIcons[2].alt}/>
+
+          {
+              socialMediaIcons.map((item, index) => {
+                  return (
+                      <img src={item.src} alt={item.alt} className="may-footer-social-media-icon" key={index}/>
+                  )
+              })
+          }
+
       </div>
       </div>
       <div>All rights reserved. may-app © 2020</div>
