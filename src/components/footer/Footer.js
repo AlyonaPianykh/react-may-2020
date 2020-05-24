666036fd5cde6d7e08dd0686e654d0478d524288
SergGrishkov <serg.grishkov@gmail.com>
2020-05-24 03:49:22 +0300

    add .gitignore completed  Task1 and Task2

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
 //todo здесь нужно сделать импорт socialMediaIcons из файла '../../constants'
+import {socialMediaIcons} from '../../constants';
 //todo здесь нужно сделать импорт стилей из './Footer.scss'
+import './Footer.scss';
 
 
+// todo здесь нужно сделать экспорт функии под названием Footer
+//   она должна вернуть div с классом "may-footer"
+//   в котором будет 2 div блока: 1й с классом "may-footer-content", второй будет без класса но с текстом 'All rights reserved. may-app © 2020'
+//   в первом div блоке должны быть вложены еще 2 блока div: 1й с текстом "Some text about authors"
+//   второй будет иметь класс "may-footer-social-media-wrapper", в него будут вложены 3 блока img в соответствии с socialMediaIcons
+//   каждый img должен иметь класс "may-footer-social-media-icon" и в пропсу src должен быть записан src из соответствующего объекта в socialMediaIcons массиве
+//   и в пропсу alt должен быть записан alt из соответствующего объекта в socialMediaIcons массиве
 
-  // todo здесь нужно сделать экспорт функии под названием Footer
-  //   она должна вернуть div с классом "may-footer"
-  //   в котором будет 2 div блока: 1й с классом "may-footer-content", второй будет без класса но с текстом 'All rights reserved. may-app © 2020'
-  //   в первом div блоке должны быть вложены еще 2 блока div: 1й с текстом "Some text about authors"
-  //   второй будет иметь класс "may-footer-social-media-wrapper", в него будут вложены 3 блока img в соответствии с socialMediaIcons
-  //   каждый img должен иметь класс "may-footer-social-media-icon" и в пропсу src должен быть записан src из соответствующего объекта в socialMediaIcons массиве
-  //   и в пропсу alt должен быть записан alt из соответствующего объекта в socialMediaIcons массиве
+export const Footer = (props) => {
+    console.log(socialMediaIcons);
+    return (
+        <div className="may-footer">
+            <div className="may-footer-content">
+                <div>
+                    <a>Some text about authors</a>
+                </div>
 
+                <div className="may-footer-social-media-wrapper">
+                    <img src={socialMediaIcons[0].src} className="may-footer-social-media-icon" alt={socialMediaIcons[0].alt}></img>
+                    <img src={socialMediaIcons[1].src} className="may-footer-social-media-icon" alt={socialMediaIcons[1].alt}></img>
+                    <img src={socialMediaIcons[2].src} className="may-footer-social-media-icon" alt={socialMediaIcons[2].alt}></img>
+                </div>
+            </div>
+
+            <div>
+                <a>All rights reserved. may-app © 2020</a>
+            </div>
+        </div>
+    )
+}
