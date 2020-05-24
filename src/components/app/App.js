32cfb010db071523a2fee805c158821f0242a831
DimaShulha <62900257+DimaShulha@users.noreply.github.com>
2020-05-24 15:15:05 +0300

    task 4 done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,8 +4,9 @@ import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
 import { postsList } from '../../constants'; // todo помимо константы postsList достать еще usersList
+import {usersList} from "../../constants";
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
-
+import {UserCard} from "../user-card/UserCard";
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -46,6 +47,13 @@ function App() {
 
       <div className="d-flex posts-container">
       {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+          {
+              usersList.map((item)=>{
+                  return(
+                      <UserCard user={item} key={item.id} />
+                  )
+              })
+          }
       </div>
 
       <Footer />
