17dc4b9ce086a31617b606123abed6dabed9ec0a
empireV <vovaempire@gmail.com>
2020-05-24 23:11:42 +0300

    done task 1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
-
-// todo: здесь нужно сделать импорт Footer из '../footer/Footer'
-
-// todo: здесь нужно сделать импорт usersList из '../../constants'
-// todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import { Footer } from '../footer/Footer';
+import {usersList} from '../../constants';
+import { UserCard } from '../user-card/UserCard';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div>content will appear here soon</div>
-      {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-
-      {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div>content will appear here soon</div>
+            <Footer/>
+            <div className="user-card-wrapper">
+                <div className="row justify-content-center">
+                    <UserCard user={usersList[0]}/>
+                    <UserCard user={usersList[1]}/>
+                </div>
+                <div className="row justify-content-center">
+                    <UserCard user={usersList[2]}/>
+                    <UserCard user={usersList[3]}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
