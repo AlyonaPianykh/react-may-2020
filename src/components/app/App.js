6c208a61b63ed0cfcd0206ffca7abf0f6d2e0142
malahit16 <malahit1616@gmail.com>
2020-05-25 13:06:50 +0300

    - footer was created including social media icons - user card was set up

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Header } from '../header/HeaderFromLecture';
-
+import { Footer } from '../footer/Footer';
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
+import { usersList } from '../../constants';
+import {UserCard} from '../user-card/UserCard';
 
 // todo: здесь нужно сделать импорт usersList из '../../constants'
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Header} from "../header/HeaderFromLecture";
 
 function App() {
   return (
@@ -15,8 +17,9 @@ function App() {
       <Header />
       <div>content will appear here soon</div>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
-
+        <Footer />
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
+      <UserCard user={usersList[0]}/>
     </div>
   );
 }
--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -1,8 +1,29 @@
 import React from 'react';
-
+import './UserCard.scss';
+import { usersList } from '../../constants';
 //todo здесь нужно сделать импорт стилей из './UserCard.scss'
 
-
+export const UserCard = (props) => {
+    console.log(props);
+    const { user } = props;
+    if (!user) return null;
+    const { first_name, last_name, email, address, _links } = user;
+    return (
+        <div className="may-user-card card">
+            <img src={usersList[0]._links.avatar.href} className="may-user-card-avatar rounded-circle"/>
+            <div className='card-body'>
+                <h4 className='may-user-card-card-title'>
+                    {first_name},
+                    {last_name}
+                </h4>
+                <div className="may-user-card-card-text">
+                    <div>{email}</div>
+                    <div>{address}</div>
+                </div>
+            </div>
+        </div>
+    )
+};
 // todo здесь нужно сделать экспорт функии под названием UserCard с аргументом props
 //  нужно деструктуризировать объект props и достать из него переменную user
 //  детальнее про деструктуризацию тут: https://learn.javascript.ru/destructuring#destrukturizatsiya-obekta
