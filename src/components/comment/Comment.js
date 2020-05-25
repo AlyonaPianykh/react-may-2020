cc7072b6db9f911eec5241f2d2d4985b54b0b1c1
zaiets.r <epidemik1989@gmail.com>
2020-05-25 18:49:54 +0300

    HomeWork Extra is done!

---
+++ src/components/comment/Comment.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Comment.scss'
+
+export function Comment(props) {
+    const { comment } = props;
+
+    if (!comment) return null;
+    const {body, email, name} = comment;
+
+    return (
+        <div className="may-user-card card">
+
+            <div className="card-body">
+                <h4 className="card-title">{name}</h4>
+                <div className="card-text">
+                    <div>{email}</div>
+                    <div>{body}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/post-card/PostCard.js
+++ src/components/post-card/PostCard.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import DefaultImg from '../../assets/default-empty-img.png';
 import './PostCard.scss';
+import {Comment} from '../comment/Comment'
 
 export function PostCard(props) {
   console.log(props);
-  // todo: достать пропсу comments из props ниже в 9й строке
-  const { post, hasImage } = props;
+  // donetodo: достать пропсу comments из props ниже в 9й строке
+  const { post, hasImage, comments, author } = props;
 
   console.log('hasImage', hasImage);
   const { title, body } = post;
@@ -57,6 +58,9 @@ export function PostCard(props) {
         <div className="card-text body">
           {body}
         </div>
+        <footer className="blockquote-footer">{author}
+
+        </footer>
       </div>
 
     {/*  todo: здесь нужно показать массив коментариев к посту (comments), который прилетит в props
@@ -67,6 +71,12 @@ export function PostCard(props) {
           email - почта автора комментария
           body - текст комментария
     */}
+
+      {comments.map(item => {
+        return(
+          <Comment key = {item.id} comment = {item}/>
+        )
+      })}
 
     </div>
   );
