c1a478179ed753b5d011826e8b7d42c97fe19748
Sophie Pankevych <sofiapankevych11@gmail.com>
2020-05-26 16:06:52 +0300

    SofiaPankevychTask2

--- src/components/header/HeaderFromLecture.js
+++ src/components/header/HeaderFromLecture.js
@@ -11,21 +11,16 @@ export const Header = (props) => {
   return (
     <div className="may-header navbar">
       <img src={Logo} className="may-header-logo" />
-
       <div className="may-header-links-wrapper">
-        {/* todo: переписать рендеринг этих ссылок используя links.map метод */}
-        <div className="nav-item">
-          <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
+         {/*todo: переписать рендеринг этих ссылок используя links.map метод */}
+          {
+              links.map((item, index) => {
+                  return <a className='may-header-links-wrapper-link' key={item.url} href={item.url}>{item.name}</a>
+              })
+          }
       </div>
-
-      <UserInfo user={user} />
+         <UserInfo user={user} />
     </div>
-  );
+  )
 };
+
