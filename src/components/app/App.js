7d266de0b8587be8473e2817cdcd844c3d1e4869
LetSerhiiStavka <ss.serhiistavka@gmail.com>
2020-05-26 14:09:57 +0300

    Serhii Stavka done hw 1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 
 // todo: здесь нужно сделать импорт Footer из '../footer/Footer'
-
+import {Footer} from '../footer/Footer';
 // todo: здесь нужно сделать импорт usersList из '../../constants'
+import {usersList} from '../../constants/index';
 // todo: здесь нужно сделать импорт UserCard из '../user-card/UserCard'
+import {UserCard} from '../user-card/UserCard';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+export default App;
 function App() {
   return (
     <div className="App">
       <Header />
       <div>content will appear here soon</div>
+        <UserCard user={usersList[0]}/>
+        <Footer/>
       {/*  todo: здесь нужно использовать Footer по аналогии с Header */}
 
       {/*  todo: здесь нужно использовать UserCard по аналогии с Header, как пропсу user передать usersList[0]*/}
     </div>
   );
 }
-
-export default App;
