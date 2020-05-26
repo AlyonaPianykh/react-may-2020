1dd62cc9ed64d52a88a4f00d360b5b8f2831dc3a
PashaShevchuk <pasha608@gmail.com>
2020-05-26 15:17:10 +0300

    - task 6 completed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,147 +1,158 @@
-import React, { Component } from 'react';
-import { Header } from '../header/HeaderFromLecture';
-import { Footer } from '../footer/Footer';
-import { PanelFromLecture } from '../panel/PanelFromLecture';
+import React, {Component} from 'react';
+import {Header} from '../header/HeaderFromLecture';
+import {Footer} from '../footer/Footer';
+import {PanelFromLecture} from '../panel/PanelFromLecture';
 
-import TestCard, { PostCard as Card } from '../post-card/PostCard';
-import { allComments, postsList, usersList } from '../../constants';
+import {PostCard as Card} from '../post-card/PostCard';
+import {allComments, postsList, usersList} from '../../constants';
 
-// todo 0) тут мы делаем импорт дропдауна (уже сделан)
-import { DropDown } from '../dropdown/DropDown';
+// done_todo 0) тут мы делаем импорт дропдауна (уже сделан)
+import {DropDown} from '../dropdown/DropDown';
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-// todo 1) вот наши опции сортировки, мы будем их использовать в DropDown (уже объявлены)
+// done_todo 1) вот наши опции сортировки, мы будем их использовать в DropDown (уже объявлены)
 const sortingOptions = ['Sort By Default', 'Sort By Author'];
 
 class App extends Component {
-  state = {
-    posts: [...postsList],
-    // todo: 2) добавить под ключом selectedOption значение sortingOptions[0] (она будет хранить выбранную в данный моменит опцию)
-  };
+    state = {
+        posts: [...postsList],
+        // done_todo: 2) добавить под ключом selectedOption значение sortingOptions[0] (она будет хранить выбранную в данный моменит опцию)
+        selectedOption: sortingOptions[0],
+    };
 
-  renderList = () => {
-    const res = [];
+    renderList = () => {
+        const res = [];
 
-    for (let i = 0; i < 6; i++) {
-      const item = postsList[i];
-      res.push(<Card post={item} key={item.id} />);
-    }
+        for (let i = 0; i < 6; i++) {
+            const item = postsList[i];
+            res.push(<Card post={item} key={item.id}/>);
+        }
 
-    return res;
-  };
+        return res;
+    };
 
-  // todo: 3) обратите внимание на эту функцию, она уже написана,
-  //  ее надо использовать в render методе, где кнопки сортировки
-  //  передать ее в DropDown как пропсу под названием onSelect (строка 118)
-  //  попробуйте продебажить и разобраться как она работает
-  onSort = (selectedOption) => {
-    // детально про строку 42 тут: https://javascript.info/destructuring-assignment#array-destructuring
-    const [option1, option2] = sortingOptions;
+    // done_todo: 3) обратите внимание на эту функцию, она уже написана,
+    //  ее надо использовать в render методе, где кнопки сортировки
+    //  передать ее в DropDown как пропсу под названием onSelect (строка 118)
+    //  попробуйте продебажить и разобраться как она работает
+    onSort = (selectedOption) => {
+        // детально про строку 42 тут: https://javascript.info/destructuring-assignment#array-destructuring
+        const [option1, option2] = sortingOptions;
 
-      switch(selectedOption) {
-        case option1:
-          this.onSortByDefault(); // вызываем сортировку по дефолту, если 1 элемент массива sortingOptions
-          this.setState({
-            selectedOption: option1 // записываем в стейт выбранную опцию
-          });
-          break;
-        case option2:
-          this.onSortByAuthorClick(); // вызываем сортировку по автору, если 2 элемент массива sortingOptions
-          this.setState({
-            selectedOption: option1 // записываем в стейт выбранную опцию
-          });
-          break;
-        default:  break; // ничего неделаем если опция не входит в массив sortingOptions
-      }
-  };
+        switch (selectedOption) {
+            case option1:
+                this.onSortByDefault(); // вызываем сортировку по дефолту, если 1 элемент массива sortingOptions
+                this.setState({
+                    selectedOption: option1 // записываем в стейт выбранную опцию
+                });
+                break;
+            case option2:
+                this.onSortByAuthorClick(); // вызываем сортировку по автору, если 2 элемент массива sortingOptions
+                this.setState({
+                    selectedOption: option2 // записываем в стейт выбранную опцию
+                });
+                break;
+            default:
+                break; // ничего неделаем если опция не входит в массив sortingOptions
+        }
+    };
 
-  onSortByDefault = () => {
-    this.setState({
-      posts: [...postsList]
-    })
-  };
+    onSortByDefault = () => {
+        this.setState({
+            posts: [...postsList]
+        })
+    };
 
-  onSortByAuthorClick = () => {
-    const res = [...this.state.posts];
+    onSortByAuthorClick = () => {
+        const res = [...this.state.posts];
 
-    const sorted = res.sort(function (a, b) {
-      const authorA =  usersList.find(user => user.id === a.user_id);
-      const authorB =  usersList.find(user => user.id === b.user_id);
+        const sorted = res.sort(function (a, b) {
+            const authorA = usersList.find(user => user.id === a.user_id);
+            const authorB = usersList.find(user => user.id === b.user_id);
 
-      if (authorA.first_name > authorB.first_name) {
-        return 1;
-      }
-      if (authorA.first_name < authorB.first_name) {
-        return -1;
-      }
-      // a должно быть равным b
-      return 0;
-    });
+            if (authorA.first_name > authorB.first_name) {
+                return 1;
+            }
+            if (authorA.first_name < authorB.first_name) {
+                return -1;
+            }
+            // a должно быть равным b
+            return 0;
+        });
 
-    this.setState({
-      posts: sorted
-    });
-  };
+        this.setState({
+            posts: sorted
+        });
+    };
 
-  render() {
-    // todo 4) достать также в строке 92 из стейта selectedOption
-    const { posts } = this.state;
+    render() {
+        // done_todo 4) достать также в строке 92 из стейта selectedOption
+        const {posts, selectedOption} = this.state;
 
-    return (
-      <div className="App">
-        <Header />
+        return (
+            <div className="App">
+                <Header/>
 
-        <PanelFromLecture isOpenByDefault={false} >
-          Hello, world!
-        </PanelFromLecture>
+                <PanelFromLecture isOpenByDefault={false}>
+                    Hello, world!
+                </PanelFromLecture>
 
-        <PanelFromLecture label="test">
-          One more panel
-        </PanelFromLecture>
+                <PanelFromLecture label="test">
+                    One more panel
+                </PanelFromLecture>
 
-        <PanelFromLecture label="Posts">
-          <div className="d-flex">
-            Sorting:
-            <button onClick={this.onSortByAuthorClick}>By author</button>
-            <button onClick={this.onSortByDefault}>By default</button>
+                <PanelFromLecture label="Posts">
+                    <div className="d-flex">
+                        Sorting:
+                        <button
+                            onClick={this.onSortByAuthorClick}
+                            type="button"
+                            className="btn btn-secondary">By author
+                        </button>
+                        <button
+                            onClick={this.onSortByDefault}
+                            type="button"
+                            className="btn btn-secondary">By default
+                        </button>
 
-            {/* todo: тут используется дропдаун
+                        {/* done_todo: тут используется дропдаун
                  ему нужно передать в пропсы такие значение:
                  в onSelect положить this.onSort
                  в selectedOption положить selectedOption (из строки 91)
                  в options положить sortingOptions
             */}
-            <DropDown
+                        <DropDown
+                            onSelect={this.onSort}
+                            selectedOption={selectedOption}
+                            options={sortingOptions}
+                        />
+                    </div>
 
+                    <div className="d-flex posts-container">
+                        {
+                            posts.map((item, index) => {
+                                const user = usersList.find(user => user.id === item.user_id);
+                                const author = user ? `${user.first_name} ${user.last_name}` : '';
+                                const comments = allComments.filter(comment => comment.post_id === item.id);
 
-
-            />
-          </div>
-          <div className="d-flex posts-container">
-            {
-              posts.map((item, index) => {
-                const user = usersList.find(user => user.id === item.user_id);
-                const author = user ? `${user.first_name} ${user.last_name}` : '';
-                const comments = allComments.filter(comment => comment.post_id === item.id);
-
-                return <Card
-                  post={item}
-                  key={item.id}
-                  hasImage={index % 2 !== 0}
-                  author={author}
-                  comments={comments}
-                />;
-              })
-            }
-          </div>
-        </PanelFromLecture>
-        <Footer />
-      </div>
-    );
-  }
+                                return <Card
+                                    post={item}
+                                    key={item.id}
+                                    hasImage={index % 2 !== 0}
+                                    author={author}
+                                    comments={comments}
+                                />;
+                            })
+                        }
+                    </div>
+                </PanelFromLecture>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/post-card/PostCard.js
+++ src/components/post-card/PostCard.js
@@ -1,64 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import { Comment } from '../comment/Comment';
+import {Comment} from '../comment/Comment';
 import DefaultImg from '../../assets/default-empty-img.png';
 import './PostCard.scss';
 
-// todo: переписать эту компоненту, чтоб она стала классовой
+// done_todo: переписать эту компоненту, чтоб она стала классовой
 //  в ней должно появиться свойство state
 //  в котором будет флажок showComments
 //  и будет кнопка, лейба которой будет либо "show comments" либо "hide comments"
 
-export function PostCard(props) {
-  const { post, hasImage, comments = [], author } = props;
+export class PostCard extends Component {
+    state = {
+        showComments: true,
+    }
 
-  console.log('hasImage', hasImage);
-  const { title, body } = post;
+    onShowHideClick = () => {
+        this.setState({
+            showComments: !this.state.showComments
+        })
+    };
 
-  const kittyUrl = `https://cataas.com/cat/says/hello%20world!?${Math.random() * 1000}`;
+    render() {
+        const {showComments} = this.state;
+        const {post, hasImage, comments = [], author} = this.props;
+        const {title, body} = post;
+        const kittyUrl = `https://cataas.com/cat/says/hello%20world!?${Math.random() * 1000}`;
+        // const renderImage = () => {
+        //     return hasImage ? <img src={kittyUrl} alt="cat"/> : <img src={DefaultImg} alt="DefaultImg"/>;
+        // };
 
-  const renderImage = () => {
-    return hasImage ? (
-      <img src={kittyUrl} />
-    ) : (
-      <img src={DefaultImg} />
-    );
-  };
+        return (
+            <div className="may-post-card card">
 
-  return (
-    <div className="may-post-card card">
-      {hasImage && (
-        <div className="may-post-card-img" id="my-block" onClick={() => {
-          alert('ghvcdhfvbdfsjvbdf');
-        }}>
-          <img src={kittyUrl} />
-        </div>
-      )
-      }
-      {!hasImage && (
-        <div className="may-post-card-img">
-          <img src={DefaultImg} />
-        </div>
-      )}
+                <button
+                    onClick={this.onShowHideClick}
+                    type="button"
+                    className="btn btn-outline-secondary ">
+                    hide/show comment
+                </button>
+                {
+                    showComments && (<div>
 
-      <div className="card-body">
-        <h4 className="card-title title">{title}</h4>
-        <div className="card-text body">
-          {body}
-        </div>
-        <blockquote className="blockquote">
-          <footer className="blockquote-footer">Author:
-            <cite title="Source Title">{author}</cite>
-          </footer>
-        </blockquote>
-      </div>
+                        {hasImage && (<div className="may-post-card-img" id="my-block"
+                                           onClick={() => alert('You clicked on the image')}>
+                            <img src={kittyUrl} alt="cat"/>
+                        </div>)
+                        }
 
-      {!!comments.length && <label>Comments:</label> }
-      {
-        comments.map(comment => (<Comment comment={comment} key={comment.id}/>))
-      }
-    </div>
-  );
+                        {!hasImage && (<div className="may-post-card-img">
+                            <img src={DefaultImg} alt="DefaultImg"/>
+                        </div>)
+                        }
+
+                        <div className="card-body">
+                            <h4 className="card-title title">{title}</h4>
+                            <div className="card-text body">{body}</div>
+                            <blockquote className="blockquote">
+                                <footer className="blockquote-footer">Author:
+                                    <cite title="Source Title">{author}</cite>
+                                </footer>
+                            </blockquote>
+                        </div>
+
+                        {!!comments.length && <label>Comments:</label>}
+                        {
+                            comments.map(comment => (<Comment comment={comment} key={comment.id}/>))
+                        }
+
+                    </div>)
+                }
+
+            </div>
+        );
+    }
 }
 
 export default PostCard;
