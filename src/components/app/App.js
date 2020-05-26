903525f7e74b2a29fce48767064ddc2a03a3fa0e
ihor <ihor04@gmail.com>
2020-05-26 15:13:39 +0300

    for rev...

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,39 +2,29 @@ import React, { Component } from 'react';
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
 import { PanelFromLecture } from '../panel/PanelFromLecture';
-
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
 import { allComments, postsList, usersList } from '../../constants';
-
 // todo 0) тут мы делаем импорт дропдауна (уже сделан)
 import { DropDown } from '../dropdown/DropDown';
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 // todo 1) вот наши опции сортировки, мы будем их использовать в DropDown (уже объявлены)
 const sortingOptions = ['Sort By Default', 'Sort By Author'];
-
 class App extends Component {
   state = {
     posts: [...postsList],
     // todo: 2) добавить под ключом selectedOption значение sortingOptions[0] (она будет хранить выбранную в данный моменит опцию)
     selectedOption: sortingOptions[0]
   };
-
   renderList = () => {
     const res = [];
-
     for (let i = 0; i < 6; i++) {
       const item = postsList[i];
       res.push(<Card post={item} key={item.id} />);
     }
-
     return res;
   };
-
-
   // todo: 3) обратите внимание на эту функцию, она уже написана,
   //  ее надо использовать в render методе, где кнопки сортировки
   //  передать ее в DropDown как пропсу под названием onSelect (строка 118)
@@ -42,7 +32,6 @@ class App extends Component {
   onSort = (selectedOption) => {
     // детально про строку 42 тут: https://javascript.info/destructuring-assignment#array-destructuring
     const [option1, option2] = sortingOptions;
-
       switch(selectedOption) {
         case option1:
           this.onSortByDefault(); // вызываем сортировку по дефолту, если 1 элемент массива sortingOptions
@@ -59,20 +48,16 @@ class App extends Component {
         default:  break; // ничего неделаем если опция не входит в массив sortingOptions
       }
   };
-
   onSortByDefault = () => {
     this.setState({
       posts: [...postsList]
     })
   };
-
   onSortByAuthorClick = () => {
     const res = [...this.state.posts];
-
     const sorted = res.sort(function (a, b) {
       const authorA =  usersList.find(user => user.id === a.user_id);
       const authorB =  usersList.find(user => user.id === b.user_id);
-
       if (authorA.first_name > authorB.first_name) {
         return 1;
       }
@@ -82,34 +67,27 @@ class App extends Component {
       // a должно быть равным b
       return 0;
     });
-
     this.setState({
       posts: sorted
     });
   };
-
   render() {
     // todo 4) достать также в строке 92 из стейта selectedOption
     const { posts, selectedOption } = this.state;
-
     return (
       <div className="App">
         <Header />
-
         <PanelFromLecture isOpenByDefault={false} >
           Hello, world!
         </PanelFromLecture>
-
         <PanelFromLecture label="test">
           One more panel
         </PanelFromLecture>
-
         <PanelFromLecture label="Posts">
           <div className="d-flex">
             Sorting:
             <button onClick={this.onSortByAuthorClick}>By author</button>
             <button onClick={this.onSortByDefault}>By default</button>
-
             {/* todo: тут используется дропдаун
                  ему нужно передать в пропсы такие значение:
                  в onSelect положить this.onSort
@@ -120,9 +98,6 @@ class App extends Component {
                 onSelect={this.onSort}
                 selectedOption={selectedOption}
                 options={sortingOptions}
-
-
-
             />
           </div>
           <div className="d-flex posts-container">
@@ -131,7 +106,6 @@ class App extends Component {
                 const user = usersList.find(user => user.id === item.user_id);
                 const author = user ? `${user.first_name} ${user.last_name}` : '';
                 const comments = allComments.filter(comment => comment.post_id === item.id);
-
                 return <Card
                   post={item}
                   key={item.id}
--- src/components/dropdown/DropDown.js
+++ src/components/dropdown/DropDown.js
@@ -5,45 +5,32 @@ import './DropDown.scss';
 export class DropDown extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       isOpen: false
     };
   }
-
 // donetodo:
 //  добавить state, в котором будет булевая пропертя isOpen по умолчанию = false
 //  на базе нее будут рендериться или нет опции дропдауна
-
   toggle = () => {
     // donetodo: здесь должно быть открывание/закрывание дропдауна с помощью this.setState
     //  сделать так же как в компоненте components/panel/PanelFromLecture в методе onClick
-    this.setState({
-    isOpen: !this.state.isOpen
-  })
+    this.setState({isOpen: !this.state.isOpen})
   };
-
   onOptionSelect = (event) => {
     // todo:
     //  достать из this.props функцию onSelect
-    const { onSelect } = this.props;
-
-
-    // todo:
+    const {onSelect} = this.props;
+        // todo:
     //  объявить переменную value, записать в нее  event.target.getAttribute('data-value');
     //  вывести console.log(value) и убедиться что в консоли показывается текст выбранной опции
-
-    const value = event.target.getAttribute();
+    const value = event.target.getAttribute('data-value');
     console.log(value);
-
     // todo: вызвать функцию из onSelect с аргументом value
-
     onSelect(value);
-
     // todo: закрыть дропдаун, вызвав this.setState({ isOpen: false })
-    this.state({isOpen : false})
-  };
-
+    this.setState({isOpen: false});
+  }
   render() {
     // todo:
     //  достать из this.props массив опций options
@@ -53,20 +40,19 @@ export class DropDown extends Component {
     // todo:
     //  достать isOpen из this.state
     const { isOpen } = this.state;
-
     return (
       <div className="may-drop-down dropdown">
         {/* todo: показать в строке 45 выбранную опцию selectedOption  и передать в onClick this.toggle
              (нажатие на этот тег должно открывать/закрывать дропдаун)
         */}
-        <div className="dropdown-toggle" onClick={this.toggle}> {this.selectedOption} </div>
+        <div className="dropdown-toggle" onClick={this.toggle}> {selectedOption} </div>
         {
           // todo:
           //  если значение isOpen в this.state = true показываем этот блок ниже (использовать &&)
           isOpen && <div className="may-drop-down-options-wrapper dropdown-menu show">
             {/*
 
-          todo:
+          donetodo:
             //  рендерим список опций с помощью options.map(option => ....
             //  опция должна быть тегом div и иметь такие пропсы:
             //  должен быть className = "may-drop-down-options-wrapper-option dropdown-item"
@@ -74,8 +60,12 @@ export class DropDown extends Component {
             //  атрибут data-value =  значению option
             //  в event listener onClick положить значение this.onOptionSelect
             //  внутри тега показать {option} (как children)
-
-          */}
+          */
+                options.map(option => {
+                return <div key={option} onClick={this.onOptionSelect} data-value={option}
+                     className="may-drop-down-options-wrapper-option dropdown-item"> {option}</div>
+                })
+              }
           </div>
         }
       </div>
