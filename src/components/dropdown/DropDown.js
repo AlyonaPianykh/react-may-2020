0082a9157f6bb3ee3ec7fa28b174681091567e44
Ruslan <someonefromua@gmail.com>
2020-05-26 11:56:49 +0300

    task 6 is done

--- src/components/dropdown/DropDown.js
+++ src/components/dropdown/DropDown.js
@@ -6,26 +6,38 @@ export class DropDown extends Component {
 // todo:
 //  добавить state, в котором будет булевая пропертя isOpen по умолчанию = false
 //  на базе нее будут рендериться или нет опции дропдауна
+state = {
+  isOpen: false
+};
+
 
   toggle = () => {
     // todo: здесь должно быть открывание/закрывание дропдауна с помощью this.setState
     //  сделать так же как в компоненте components/panel/PanelFromLecture в методе onClick
+    this.setState({
+      isOpen: !this.state.isOpen
+        })
   };
 
   onOptionSelect = (event) => {
     // todo:
     //  достать из this.props функцию onSelect
-
-
+    const {onSelect} = this.props
+    
     // todo:
     //  объявить переменную value, записать в нее  event.target.getAttribute('data-value');
+    const value = event.target.getAttribute('data-value');
     //  вывести console.log(value) и убедиться что в консоли показывается текст выбранной опции
+    console.log(value);
 
 
     // todo: вызвать функцию из onSelect с аргументом value
-
+    onSelect(value);
 
     // todo: закрыть дропдаун, вызвав this.setState({ isOpen: false })
+    this.setState({
+      isOpen: false
+    })
   };
 
   render() {
@@ -33,22 +45,24 @@ export class DropDown extends Component {
     //  достать из this.props массив опций options
     //  он должен быть по умолчанию пуст, т.е. = [] (дефолтный параметр при деструктуризации) и
     //  достать также в пропсах selectedOption (выбранная в данный момент опция)
-
+const {options = [], selectedOption} = this.props
     // todo:
     //  достать isOpen из this.state
+    const {isOpen} = this.state;
 
     return (
       <div className="may-drop-down dropdown">
         {/* todo: показать в строке 45 выбранную опцию selectedOption  и передать в onClick this.toggle
              (нажатие на этот тег должно открывать/закрывать дропдаун)
         */}
-        <div className="dropdown-toggle">{/* тут будет выбранная опция*/}</div>
+        <div className="dropdown-toggle" onClick={this.toggle}>{/* тут будет выбранная опция*/}{selectedOption}</div>
 
         {
           // todo:
           //  если значение isOpen в this.state = true показываем этот блок ниже (использовать &&)
-          <div className="may-drop-down-options-wrapper dropdown-menu show">
+          isOpen && <div className="may-drop-down-options-wrapper dropdown-menu show">
             {/*
+
           todo:
             //  рендерим список опций с помощью options.map(option => ....
             //  опция должна быть тегом div и иметь такие пропсы:
@@ -58,6 +72,14 @@ export class DropDown extends Component {
             //  в event listener onClick положить значение this.onOptionSelect
             //  внутри тега показать {option} (как children)
           */}
+            {options.map(option => <div
+                className={`may-drop-down-options-wrapper-option dropdown-item ${option === selectedOption? 'active': ''}`}
+                key={option}
+                data-value={option}
+                onClick={this.onOptionSelect}>
+              {option}
+                </div>
+                )}
           </div>
         }
       </div>
