084ebc40eee6afeb706a9ce1d7f38c66957c1840
LetSerhiiStavka <ss.serhiistavka@gmail.com>
2020-05-26 20:40:19 +0300

    Done HW 2

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,66 +3,63 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
-// todo: достать в строке 7 массив allComments из констант
-import { postsList } from '../../constants'; // todo помимо константы postsList достать еще usersList
+import { postsList } from '../../constants';
+// todo помимо константы postsList достать еще usersList
+import {usersList} from "../../constants";
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
-
+import {UserCard} from "../user-card/UserCard";
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const renderList = () => {
-    const res = [];
+    const renderList = () => {
+        const res = [];
 
-    for (let i = 0; i < 6; i++) {
-      const item = postsList[i];
-      res.push(<Card post={item} key={item.id}/>)
-    }
-
-    return res;
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      <div>content will appear here soon</div>
-
-      {/*<div className="d-flex posts-container">*/}
-      {/*  {*/}
-      {/*    renderList()*/}
-      {/*  }*/}
-      {/*</div>*/}
-
-      <div className="d-flex posts-container">
-        {
-          postsList.map((item, index) => {
-            const odd = index % 2 !== 0;
-            // todo: найти в массиве usersList пользователя, айди которого равно user_id в посте (т.е. в данном случае item)
-            //  для этого можно использовать функцию массива find или findIndex
-            //  передать имя и фамилию пользователя как пропсу author в Card
-            //  использовать для этого стринговый литерал ``
-            //  в Card под телом поста срендерить имя автора, используя blockquote-footer класс из бутстрапа
-            //  пример тут: https://hackerthemes.com/bootstrap-cheatsheet/#blockquote-footer
-
-
-            // todo: найти в массиве allComments комментарии, post_id которых = id поста  (т.е. в данном случае item.id)
-            //  для этого можно использовать метод массива filter
-            //  передать этот массив в Card как пропсу под названиес comments
-
-              return <Card post={item} key={item.id} hasImage={odd} />
-          })
+        for (let i = 0; i < 6; i++) {
+            const item = postsList[i];
+            res.push(<Card post = {item} key = {item.id}/>)
         }
-      </div>
 
-      <div className="d-flex posts-container">
-      {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+        return res;
+    };
 
-      </div>
+    return (
+        <div className = "App">
+            <Header />
+            <div>content will appear here soon</div>
 
-      <Footer />
-    </div>
-  );
+            {/*<div className="d-flex posts-container">*/}
+            {/*  {*/}
+            {/*    renderList()*/}
+            {/*  }*/}
+            {/*</div>*/}
+
+            <div className = "d-flex posts-container">
+                {
+                    postsList.map ((item, index) => {
+                        const odd = index % 2 !== 0;
+
+                        console.log(odd)
+                        return <Card post = {item} key = {item.id} hasImage = {odd} />
+                    })
+                }
+            </div>
+
+            <div className = "d-flex posts-container">
+                {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+                {
+                    usersList.map((item) => {
+                        return(
+                            <UserCard user = {item} key = {item.id} />
+                        )
+                    })
+                }
+            </div>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -23,3 +23,5 @@ export function UserCard(props) {
     </div>
   );
 }
+
+
