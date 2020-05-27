a6cc3882f78adea9764e199dbdffac52b15aeb71
Sanya911s <sanity2427@gmail.com>
2020-05-27 09:43:41 +0300

    -make dropdown menu

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,20 +6,21 @@ import { PanelFromLecture } from '../panel/PanelFromLecture';
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
 import { allComments, postsList, usersList } from '../../constants';
 
-// todo 0) тут мы делаем импорт дропдауна (уже сделан)
+// donetodo 0) тут мы делаем импорт дропдауна (уже сделан)
 import { DropDown } from '../dropdown/DropDown';
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-// todo 1) вот наши опции сортировки, мы будем их использовать в DropDown (уже объявлены)
+// donetodo 1) вот наши опции сортировки, мы будем их использовать в DropDown (уже объявлены)
 const sortingOptions = ['Sort By Default', 'Sort By Author'];
 
 class App extends Component {
   state = {
     posts: [...postsList],
-    // todo: 2) добавить под ключом selectedOption значение sortingOptions[0] (она будет хранить выбранную в данный моменит опцию)
+    // donetodo: 2) добавить под ключом selectedOption значение sortingOptions[0] (она будет хранить выбранную в данный моменит опцию)
+    selectedOption: sortingOptions[0],
   };
 
   renderList = () => {
@@ -33,7 +34,7 @@ class App extends Component {
     return res;
   };
 
-  // todo: 3) обратите внимание на эту функцию, она уже написана,
+  // donetodo: 3) обратите внимание на эту функцию, она уже написана,
   //  ее надо использовать в render методе, где кнопки сортировки
   //  передать ее в DropDown как пропсу под названием onSelect (строка 118)
   //  попробуйте продебажить и разобраться как она работает
@@ -51,7 +52,7 @@ class App extends Component {
         case option2:
           this.onSortByAuthorClick(); // вызываем сортировку по автору, если 2 элемент массива sortingOptions
           this.setState({
-            selectedOption: option1 // записываем в стейт выбранную опцию
+            selectedOption: option2 // записываем в стейт выбранную опцию
           });
           break;
         default:  break; // ничего неделаем если опция не входит в массив sortingOptions
@@ -87,8 +88,9 @@ class App extends Component {
   };
 
   render() {
-    // todo 4) достать также в строке 92 из стейта selectedOption
+    // donetodo 4) достать также в строке 92 из стейта selectedOption
     const { posts } = this.state;
+    const { selectedOption } = this.state;
 
     return (
       <div className="App">
@@ -108,17 +110,13 @@ class App extends Component {
             <button onClick={this.onSortByAuthorClick}>By author</button>
             <button onClick={this.onSortByDefault}>By default</button>
 
-            {/* todo: тут используется дропдаун
+            {/* donetodo: тут используется дропдаун
                  ему нужно передать в пропсы такие значение:
                  в onSelect положить this.onSort
                  в selectedOption положить selectedOption (из строки 91)
                  в options положить sortingOptions
             */}
-            <DropDown
-
-
-
-            />
+            <DropDown onSelect={this.onSort} selectedOption={selectedOption} options={sortingOptions} />
           </div>
           <div className="d-flex posts-container">
             {
--- src/components/panel/PanelFromLecture.js
+++ src/components/panel/PanelFromLecture.js
@@ -4,60 +4,60 @@ import Chevron from '../../assets/chevron-bottom.png';
 import './Panel.scss';
 
 export class PanelFromLecture extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    const { isOpenByDefault } = props;
+        const {isOpenByDefault} = props;
 
-    this.state = {
-      isOpen: isOpenByDefault,
-      inputVal: 'test'
+        this.state = {
+            isOpen: isOpenByDefault,
+            inputVal: 'test'
+        };
+    }
+
+    test = 'test';
+
+    onClick = () => {
+        console.log('test');
+
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     };
-  }
 
-  test = 'test';
+    onChange = (event) => {
+        debugger
+        console.log(event);
+        console.log(event.target.value);
 
-  onClick = () => {
-    console.log('test');
+        this.setState({
+            inputVal: event.target.value
+        });
+    };
 
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  };
+    render() {
+        console.log('render');
+        const {children, label} = this.props;
+        const {isOpen} = this.state;
 
-  onChange = (event) => {
-    debugger
-    console.log(event)
-    console.log(event.target.value)
+        return (
+            <div className="may-panel card">
 
-    this.setState({
-      inputVal: event.target.value
-    });
-  };
-
-  render () {
-    console.log('render')
-    const { children, label } = this.props;
-    const {isOpen} = this.state;
-
-    return (
-      <div className="may-panel card">
-
-        {/*<input value={this.state.inputVal} onChange={this.onChange}/>*/}
-        <div className="may-panel-header card-header">
-          <div>{label}</div>
-          <div className={`may-panel-header-chevron ${isOpen ? 'up' : ''}`} onClick={this.onClick}>
-            <img src={Chevron} alt="chevron arrow"/>
-          </div>
-        </div>
-        {
-          isOpen && (
-            <div className="card-body">
-              { children }
+                {/*<input value={this.state.inputVal} onChange={this.onChange}/>*/}
+                <div className="may-panel-header card-header">
+                    <div>{label}</div>
+                    <div className={`may-panel-header-chevron ${isOpen ? 'up' : ''}`} onClick={this.onClick}>
+                        <img src={Chevron} alt="chevron arrow"/>
+                    </div>
+                </div>
+                {
+                    isOpen && (
+                        <div className="card-body">
+                            {children}
+                        </div>
+                    )
+                }
             </div>
-          )
-        }
-      </div>
-    );
-  }
+        );
+    }
 }
