24827f8219d6028273df9905c099344bcbc98de5
ValeriyGurskiy <ya.zazaza77@gmail.com>
2020-05-27 18:08:43 +0300

    done task 3&4

--- src/components/header/HeaderFromLecture.js
+++ src/components/header/HeaderFromLecture.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { links, user } from '../../constants';
+import {links, user, usersList} from '../../constants';
 import Logo from '../../assets/react.png';
 
 import { UserInfo } from '../user-info/UserInfoFromLecture';
@@ -13,16 +13,17 @@ export const Header = (props) => {
       <img src={Logo} className="may-header-logo" />
 
       <div className="may-header-links-wrapper">
-        {/* todo: переписать рендеринг этих ссылок используя links.map метод */}
-        <div className="nav-item">
-          <a href={links[0].url} className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[1].url} className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
+          {
+              links.map((value, index) =>
+              {
+                  return (
+                      <div key={index} className="nav-item">
+                          <a href={value.url} className="may-header-links-wrapper-link nav-link">{value.name}</a>
+                      </div>
+                  )
+              })
+          }
+
       </div>
 
       <UserInfo user={user} />
