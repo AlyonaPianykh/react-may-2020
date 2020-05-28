5a3be5e0fb41726ce69f7147b5eb091c563fe28a
ihor <ihor04@gmail.com>
2020-05-28 12:58:27 +0300

    for rev...

--- src/components/post-card/PostCard.js
+++ src/components/post-card/PostCard.js
@@ -5,7 +5,7 @@ import { Comment } from '../comment/Comment';
 import DefaultImg from '../../assets/default-empty-img.png';
 import './PostCard.scss';
 
-// todo: делаем открывание/закрывание комментариев по кнопочке и добавляем лоадинг индикатор на загрузку комментариев
+// donetodo: делаем открывание/закрывание комментариев по кнопочке и добавляем лоадинг индикатор на загрузку комментариев
 
 export class PostCard extends PureComponent {
   constructor(props) {
@@ -14,11 +14,15 @@ export class PostCard extends PureComponent {
   }
 
   state = {
-    comments: []
-    // todo в стейт добавить флажок isCommentsLoading, который будет означать идет ли загрузка в данный момент, по умолчанию false
-    // todo в стейт добавить флажок commentsLoaded, который будет означать загрузились ли коментарии, по умолчанию false
-    // todo в стейт добавить флажок showComments, который будет означать отображается ли секция с коментариями в данный момент, по умолчанию false
-    // todo в стейт добавить строку error, чтоб хранить значения ошибок, если возникнут
+    comments: [],
+    isCommentLoading: false,
+    commentsLoaded: false,
+    showComments: false,
+    error: null
+    // donetodo в стейт добавить флажок isCommentsLoading, который будет означать идет ли загрузка в данный момент, по умолчанию false
+    // donetodo в стейт добавить флажок commentsLoaded, который будет означать загрузились ли коментарии, по умолчанию false
+    // donetodo в стейт добавить флажок showComments, который будет означать отображается ли секция с коментариями в данный момент, по умолчанию false
+    // donetodo в стейт добавить строку error, чтоб хранить значения ошибок, если возникнут
   };
 
   componentDidMount() {
@@ -41,8 +45,15 @@ export class PostCard extends PureComponent {
 
   loadComments = async (postId) => {
 
-    //  todo поменять стейт так, чтоб было понятно что секция с комментариями открыта и началась загрузка
-    //  todo т.е. isCommentsLoading и showComments станут true
+    this.setState({
+      isCommentLoading: true,
+      showComments: true
+    })
+
+
+    //  donetodo поменять стейт так, чтоб было понятно что секция с комментариями открыта и началась загрузка
+    //  donetodo т.е. isCommentsLoading и showComments станут true
+
 
     let response = await fetch(`https://gorest.co.in/public-api/comments?access-token=${accessToken}&post_id=${postId}`);
 
@@ -54,23 +65,36 @@ export class PostCard extends PureComponent {
 
       if (Array.isArray(result)) { // во время выполнения запроса м.б. вариант когда result не массив
         this.setState({
-          // todo указать, что лоадинг закончился, т.е. isCommentsLoading будет false,
+          // donetodo указать, что лоадинг закончился, т.е. isCommentsLoading будет false,
           //  а commentsLoaded станет true (т.е. запрос был выполнен)
           //  в error записываем пустую строку '' - показываем, что ошибки нет
+          isCommentsLoading: false,
+          commentsLoaded: true,
+          error: '',
           comments: result
         });
       }
     } else {
-      // todo поменять стейт так, чтоб
+      // donetodo поменять стейт так, чтоб
       //  лоадинг закончился, т.е. isCommentsLoading будет false,
       //  а commentsLoaded станет false (т.е. запрос не был выполнен ввиду ошибки)
       //  в error пойдет значение response.status
       //  и закроем секцию коментариев т.е. commentsSectionExpanded будет false
+      this.setState({
+        isCommentsLoading: false,
+        commentsLoaded: false,
+        error: response.status,
+        commentsSectionExpanded: false
+      });
     }
-  };
+  }
+
 
   onToggleComments = () => {
-    // todo
+    this.setState({
+      showComments: !this.state.showComments
+    });
+    // donetodo
     //    меняем в стейт значение showComments на противоположное (по аналогии как мы делали isOpen для PanelFromLecture
   };
 
@@ -84,9 +108,9 @@ export class PostCard extends PureComponent {
   render() {
     const { post, hasImage, author = '', className = '' } = this.props;
     const { title, body } = post;
-    {/* todo с помощью  деструктуризации достать из this.state проперти showComments, error, isCommentsLoading, commentsLoaded */
+    {/* donetodo с помощью  деструктуризации достать из this.state проперти showComments, error, isCommentsLoading, commentsLoaded */
     }
-    const { comments } = this.state;
+    const { comments, showComments, error, isCommentsLoading, commentsLoaded } = this.state;
 
     const kittyUrl = `https://cataas.com/cat/says/hello%20world!?${Math.random() * 1000}`;
 
@@ -109,36 +133,44 @@ export class PostCard extends PureComponent {
             </footer>
           </blockquote>
         </div>
-
         {
-          // todo: добавить label, в котором как children будет если showComments = true - надпись "Hide comments",
+          // donetodo: добавить label, в котором как children будет если showComments = true - надпись "Hide comments",
           //    иначе - "Show comments"
           //    повесить на нее onClick событие this.onToggleComments
           //    как класс задать ей "btn btn-link"
-        }
-        {/* todo создать div который будет как children содержать error, если !!error */}
+          <label  className="btn btn-link" onClick={this.onToggleComments}> {!!showComments ? "Hide comments" : " Show comments"} </label>
 
-        {/* todo в строке ниже изменить условие если showComments = true то показываем <label>Comments:</label>*/}
-        {!!comments.length && <label>Comments:</label>}
+        }
+        {/* donetodo создать div который будет как children содержать error, если !!error */}
+        <div>{!!error ? error : ''}</div>
+        {/* donetodo в строке ниже изменить условие если showComments = true то показываем <label>Comments:</label>*/}
+        {!!showComments && <label>Comments:</label>}
         {
-          //todo если секция комментариев открыта, т.е. showComments = true
+          //donetodo если секция комментариев открыта, т.е. showComments = true
           //   и идет загрузка комментариев, т.е. isCommentsLoading = true
           // показываем лоадинг индикатор (можно просто строку с надписью "Loading comments ..." в div)
+          !!showComments && !!isCommentsLoading && (
+              <div>Loading comments....</div>
+          )
         }
         {
-          //todo если секция комментариев открыта, т.е. showComments = true
+          //donetodo если секция комментариев открыта, т.е. showComments = true
           //   но НЕ идет загрузка комментариев, т.е. isCommentsLoading = false
           //   и запрос уже был выполнен т.е. commentsLoaded = true
           //   и массив comments пустой, т.е. !comments.length
           // показываем сообщение, что нет результатов (div с надписью "No comments for this post yet" в div)
+          !!showComments && !isCommentsLoading && !!commentsLoaded && !comments.length && (
+              <div>No comments for this post yet</div>
+          )
         }
         {
-          // todo если секция комментариев открыта, т.е. showComments = true
+          // donetodo если секция комментариев открыта, т.е. showComments = true
           //   и НЕ идет загрузка комментариев, т.е. isCommentsLoading = false
           //   и запрос уже был выполнен т.е. commentsLoaded = true
           //   и массив comments НЕ пустой, т.е. !!comments.length
           //    то:
-          comments.map(comment => (<Comment comment={comment} key={comment.id} />))
+          !!showComments && !isCommentsLoading && !!commentsLoaded && !!comments.length  && (
+          comments.map(comment => (<Comment comment={comment} key={comment.id} />)))
         }
       </div>
     );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-// todo: зарегистрироваться на https://gorest.co.in и вписать свой accessToken ниже
-export const accessToken = '';
+// donetodo: зарегистрироваться на https://gorest.co.in и вписать свой accessToken ниже
+export const accessToken = 'q2VbdxssP_YPs7U26BZbOKmdu16LZPTyVkXE';
 
 export const user = {
   firstName: 'Poison',
