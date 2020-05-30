70c28578684cf18caf61362ad5df6ce9c5eb19fc
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2020-05-30 13:15:08 +0200

    end second task

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { Header } from '../header/HeaderFromLecture';
 import { Footer } from '../footer/Footer';
 import TestCard, { PostCard as Card } from '../post-card/PostCard';
-import { postsList } from '../../constants'; // todo помимо константы postsList достать еще usersList
+import { postsList,usersList } from '../../constants'; // todo помимо константы postsList достать еще usersList
 // todo: тут сделать импорт  UserCard из components/user-card/UserCard
+import {UserCard} from "../user-card/UserCard";
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -37,15 +38,21 @@ function App() {
         {
           postsList.map((item, index) => {
             const odd = index % 2 !== 0;
+            if (index < 5) return <Card post={item} key={item.id} hasImage={odd} />
 
-            console.log(odd)
-              return <Card post={item} key={item.id} hasImage={odd} />
+            console.log(odd);
+              // return <Card post={item} key={item.id} hasImage={odd} />
           })
         }
       </div>
 
       <div className="d-flex posts-container">
       {/*  todo: срендерить тут список пользователей, используя компонент UserCard */}
+          {
+              usersList.map((item,index) => {
+                  return <UserCard user={item} key={item.id}/>
+              })
+          }
       </div>
 
       <Footer />
