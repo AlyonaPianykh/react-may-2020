b9adc6440e34c0a9f7c6e941cffaf43b652e616a
PlugAndP1ay <sasha12101995bor@gmail.com>
2020-05-30 17:12:44 +0300

    done task4 and loading spinner

--- src/components/post-card/PostCard.js
+++ src/components/post-card/PostCard.js
@@ -158,7 +158,10 @@ export class PostCard extends PureComponent {
                     //donetodo если секция комментариев открыта, т.е. showComments = true
                     //   и идет загрузка комментариев, т.е. isCommentsLoading = true
                     // показываем лоадинг индикатор (можно просто строку с надписью "Loading comments ..." в div)
-                    showComments && isCommentsLoading && <div>"Loading comments ..."</div>
+                    showComments && isCommentsLoading &&
+                    <div className="loadingspinner-container">
+                        <div className="loadingspinner-body"/>
+                    </div>
                 }
 
                 {
