9c21e33f417127deabb11f71550008e830fe069c
rostyslav4081 <rostuslav9913@gmail.com>
2020-06-02 17:04:03 +0300

    hw_4_SK

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-// todo: зарегистрироваться на https://gorest.co.in и вписать свой accessToken ниже
-export const accessToken = '';
+// donetodo: зарегистрироваться на https://gorest.co.in и вписать свой accessToken ниже
+export const accessToken = 'F6lrmsjoeP_DiF1e6nEFvw-fgXkJEjPeAMWO';
 
 export const user = {
   firstName: 'Poison',
