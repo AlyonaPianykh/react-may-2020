8762fa4a18ea9f898dbbc7e33f03f6c1ef5d9d0e
margl13 <62597926+margl13@users.noreply.github.com>
2020-06-04 19:02:36 +0200

    Tasks from homework 5 are done

--- src/components/user-form/AddUserForm.js
+++ src/components/user-form/AddUserForm.js
@@ -5,43 +5,51 @@ class AddUserForm extends Component {
   firstNameRef = createRef();
   lastNameRef = createRef();
   emailNameRef = createRef();
-  // todo 1: добавить ref для address
-
+  // donetodo 1: добавить ref для address
+  addressNameRef = createRef();
 
   state = {
     warning: ''
   };
 
   onSubmit = (e) => {
-    // todo 1: достать из props функцию добавления пользователя onUserAdd
+    // donetodo 1: достать из props функцию добавления пользователя onUserAdd
     e.preventDefault();
-
+    const {onUserAdd} = this.props;
     console.log(this.firstNameRef.current.value);
     console.log(this.lastNameRef.current.value);
 
-    const firstName = this.firstNameRef.current.value;
-    const lastName = this.lastNameRef.current.value;
+    const first_name = this.firstNameRef.current.value;
+    const last_name = this.lastNameRef.current.value;
     const email = this.emailNameRef.current.value;
-
+    const address = this.addressNameRef.current.value;
     const pattern = /\d+/;
 
-    if (pattern.test(firstName) || pattern.test(lastName))  {
+    if (pattern.test(first_name) || pattern.test(last_name))  {
       this.setState({
         warning: 'please don\'t put numbers '
       });
       return;
     }
 
-    console.log(firstName, lastName, email);
-    // todo 1: использовать функцию onUserAdd для создания нового пользователя здесь
+    console.log(first_name, last_name, email);
+    // donetodo 1: использовать функцию onUserAdd для создания нового пользователя здесь
     //  у этого объекта должны быть проперти first_name, last_name, address, email
-
-    // todo 3: зачистить форму, вызвав функцию onReset
+    onUserAdd && onUserAdd({
+      first_name, last_name, address, email
+    });
+    // donetodo 3: зачистить форму, вызвав функцию onReset
+    this.onReset();
   };
 
-  // todo 3: добавить функцию onReset, которая будет зачищать поля формы
+  // donetodo 3: добавить функцию onReset, которая будет зачищать поля формы
   //  обратите внимание, что тут нет стейта и мы обращаемся к инпутам с помощью ref
-
+    onReset = () => {
+      this.firstNameRef.current.value = '';
+      this.lastNameRef.current.value = '';
+      this.emailNameRef.current.value = '';
+      this.addressNameRef.current.value = '';
+    };
   focusInput = () => {
     console.log('focused');
     this.lastNameRef.current.focus()
@@ -85,15 +93,24 @@ class AddUserForm extends Component {
         </div>
 
         {/*
-        todo 1: добавить инпут для ввода адресса
+        donetodo 1: добавить инпут для ввода адресса
               передать ему атрибут под название ref наш созданный в строке 8 ref
         */}
-
+        <div className="form-group">
+          <label htmlFor="lastNameInput">Address:</label>
+          <input
+              ref={this.addressNameRef}
+              type="text"
+              className="form-control"
+              id="lastNameInput"
+              placeholder="Example input"
+          />
+        </div>
         <button type='submit' className="btn btn-primary m-2">Add </button>
         <button type='button' onClick={this.focusInput}>focus</button>
-
+        <button type='button' className="btn btn-secondary m-2" onClick={this.onReset}>Reset</button>
       {/*
-      todo 3: добавить кнопку, которая по нажатию будет будет вызывать метод onReset
+      donetodo 3: добавить кнопку, которая по нажатию будет будет вызывать метод onReset
       */}
       </form>
     );
