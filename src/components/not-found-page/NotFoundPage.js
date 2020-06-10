bfc2f32580f42ede47e7cc0393e01ab89cdbfc11
Maksym-Kabak <maksitheslayer@gmail.com>
2020-06-10 12:34:17 +0300

    Maks Kabak task 9 done! lecture 6

---
+++ src/components/not-found-page/NotFoundPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const NotFoundPage = (props) => {
+    const { history } = props;
+    const toHomePage = () => {
+        history.push('/home');
+    };
+    // dtodo 1: добавить кнопку навигации на back to home page
+    //  подумайте, как для этого надо изменить эту страницу
+    return (
+        <div>
+            <h1 className="text-center">Page not found ...</h1>
+            <button className="btn btn-primary m-2" type="button" onClick={toHomePage}> Go back to homepage</button>
+        </div>
+
+    );
+};
