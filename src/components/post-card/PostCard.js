bef85dc4d9691bbe31d79b840a53ee4348ea1a00
Pavel-coder <57906848+Pavel-coder@users.noreply.github.com>
2020-06-13 15:08:36 +0300

    Done task 6, but Posts is not loaded, can't find error

--- src/components/post-card/PostCard.js
+++ src/components/post-card/PostCard.js
@@ -1,5 +1,7 @@
 import React, { Component, PureComponent } from 'react';
-// todo 3: сделать импорт Link из react-router-dom
+// dtodo 3: сделать импорт Link из react-router-dom
+import {Link} from 'react-router-dom';
+import {withRouter} from 'react-router';
 import { accessToken } from '../../constants';
 import { Comment } from '../comment/Comment';
 import DefaultImg from '../../assets/default-empty-img.png';
@@ -78,9 +80,8 @@ class PostCard extends PureComponent {
   // }
 
   render() {
-    //todo 3 : достать ниже url из  props.match по аналогии с UserCard строка 7
-    const { post, hasImage, author = '', className = '' } = this.props;
-
+    //dtodo 3 : достать ниже url из  props.match по аналогии с UserCard строка 7
+    const { post, hasImage, author = '', className = '',match: {url} } = this.props;
     if (!post) {
       console.log('post is not defined');
       return null;
@@ -113,11 +114,11 @@ class PostCard extends PureComponent {
         }
         { !!error &&  <div>{error}</div> }
 
-        {/* todo 3 : добавить ссылку Link на урлу с айди поста, где будут детали поста
+        {/* dtodo 3 : добавить ссылку Link на урлу с айди поста, где будут детали поста
                     по аналогии с 24 строкой в UserCard
                     рендерить линку только если id из пропсов-объекта match-объекта params не найдено (не существует)
         */}
-
+<Link to ={`${url}/${post.id}`}>Show Details</Link>
         { showComments && !!comments.length && <label>Comments:</label> }
         { showComments && isCommentsLoading && <div>Loading...</div> }
         {
@@ -136,5 +137,5 @@ class PostCard extends PureComponent {
   }
 }
 
-// todo 3: подвязать PostCard с помощью withRouter к роутеру по аналогии с UserCard
-export default PostCard;
+// dtodo 3: подвязать PostCard с помощью withRouter к роутеру по аналогии с UserCard
+export default withRouter(PostCard);
