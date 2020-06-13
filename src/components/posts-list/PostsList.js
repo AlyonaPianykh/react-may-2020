cee8bd0561e46a29a548ff72c030d6be31c16ab8
VolodymyrBomko <vovabomko@gmail.com>
2020-06-13 15:54:35 +0300

    task 6 finish

--- src/components/posts-list/PostsList.js
+++ src/components/posts-list/PostsList.js
@@ -1,53 +1,82 @@
-import React, { Component } from 'react';
-import { accessToken, usersList } from '../../constants';
+import React, {Component} from 'react';
+import {accessToken, usersList} from '../../constants';
 import PostCard from '../post-card/PostCard';
 
 class PostsList extends Component {
-  state = {
-    posts: []
-    // todo 2: добавить isLoading индикатор
-  };
+    state = {
+        posts: [],
+        // dtodo 2: добавить isLoading индикатор
+        isLoading: false
+    };
 
-  componentDidMount() {
-    // todo 2: вызвать загрузку постов
-  }
+    componentDidMount() {
+        // dtodo 2: вызвать загрузку постов
+        this.loadPosts()
+    }
 
-  loadPosts = async () => {
-    // todo 2:
-    //  проверьте лежит ли ваш accessToken в constants/index.js
-    //  прежде чем отправить запрос - включите в true флажок загрузки в стейте isLoading
-    //  выполните fetch запрос за поcтами на `https://gorest.co.in/public-api/posts?access-token=${accessToken}`
-    //  похожий запрос выполнялся в компоненте PostCard в функции loadComments
-    //  результат выполнения запроса нужно положить в стейт в posts
-    //  когда запрос выполнится - не забудьте поменять индикатор загрузки isLoading на false
-  };
+    loadPosts = async () => {
+        // dtodo 2:
+        //  проверьте лежит ли ваш accessToken в constants/index.js
+        //  прежде чем отправить запрос - включите в true флажок загрузки в стейте isLoading
+        //  выполните fetch запрос за поcтами на `https://gorest.co.in/public-api/posts?access-token=${accessToken}`
+        //  похожий запрос выполнялся в компоненте PostCard в функции loadComments
+        //  результат выполнения запроса нужно положить в стейт в posts
+        //  когда запрос выполнится - не забудьте поменять индикатор загрузки isLoading на false
+        this.setState({
+            isLoading: true,
+        });
 
-  render() {
-    // todo 2: достать также лоадинг индикатор из стейта
-    const { posts } = this.state;
+        let response = await fetch(`https://gorest.co.in/public-api/posts?access-token=${accessToken}`);
 
-    return (
-      <div>
-        <div>Posts page</div>
+        if (response.ok) {
+            let json = await response.json();
 
-        {/* todo 2: ниже добавить проверку если сейчас идет загрука то показываем лоадинг индикатор (как в задании 7)
+            const {result} = json;
+
+            if (Array.isArray(result)) { // во время выполнения запроса м.б. вариант когда result не массив
+                this.setState({
+                    isLoading: false,
+                    error: '',
+                    posts: result || [] // изменена проверка, если results существовать не будет - закидываем пустой массив
+                });
+            }
+        } else {
+            this.setState({
+                isLoading: false,
+                error: response.status,
+            });
+        }
+    };
+
+    render() {
+        // dtodo 2: достать также лоадинг индикатор из стейта
+        const {posts, isLoading} = this.state;
+
+        return (
+            <div>
+                <div>Posts page</div>
+
+                {/* dtodo 2: ниже добавить проверку если сейчас идет загрука то показываем лоадинг индикатор (как в задании 7)
                     если загрузка не идет то показываем список постов
         */}
-        {
-          posts.map((item) => {
-            const user = usersList.find(user => user.id === item.user_id);
-            const author = user ? `${user.first_name} ${user.last_name}` : '';
+                {
+                    isLoading && <div>loading</div>
+                }
+                {
+                    !isLoading && posts.map((item) => {
+                        const user = usersList.find(user => user.id === item.user_id);
+                        const author = user ? `${user.first_name} ${user.last_name}` : '';
 
-            return <PostCard
-              post={item}
-              key={item.id}
-              author={author}
-            />;
-          })
-        }
-      </div>
-    );
-  }
+                        return <PostCard
+                            post={item}
+                            key={item.id}
+                            author={author}
+                        />;
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default PostsList;
