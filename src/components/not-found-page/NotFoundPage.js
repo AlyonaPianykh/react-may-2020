50c14eda3397dff1ff9e3ecb2db93e445e91ba92
Kostyantyn <32463063+RudenkoKostya@users.noreply.github.com>
2020-06-14 18:52:43 +0300

    lecture-6-hw-done

---
+++ src/components/not-found-page/NotFoundPage.js
@@ -0,0 +1,14 @@
+// donetodo 1: вынести эту функцию в отдельную компоненту: сощздать папку, js файл
+import React from "react";
+import {Link} from "react-router-dom";
+
+const NotFoundPage = () => {
+  // dtodo 1: добавить кнопку навигации на back to home page
+  //  подумайте, как для этого надо изменить эту страницу
+  return (
+      <div>Page not found
+        <Link to='/home'>back to home page</Link>
+      </div>)
+};
+
+export default NotFoundPage;
